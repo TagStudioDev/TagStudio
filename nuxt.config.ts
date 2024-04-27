64c8564afc6a23e3014035d73cddd182b8edc879
parthos <14004786+parthos-dev@users.noreply.github.com>
2024-04-27 16:15:44 -0500

    disable telemetry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  telemetry: { enabled: false},
   
   // Config using 
   // https://beta.tauri.app/start/frontend-configuration/nuxt/
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
