ebf2c0bb99dfc0e45a531cb358f9f5afc691f771
Andrew Arneson <Loran425@gmail.com>
2024-04-27 20:26:26 -0600

    split AddFieldModal from ts_qt.py

--- tagstudio/src/qt/modals/__init__.py
+++ tagstudio/src/qt/modals/__init__.py
@@ -1,3 +1,4 @@
 from .tag_search import TagSearchPanel
 from .build_tag import BuildTagPanel
 from .tag_database import TagDatabasePanel
+from .add_field import AddFieldModal
--- tagstudio/src/qt/ts_qt.py
+++ tagstudio/src/qt/ts_qt.py
@@ -46,7 +46,7 @@ from src.qt.main_window import Ui_MainWindow
 from src.qt.helpers import open_file, FileOpenerHelper, FileOpenerLabel
 from src.qt.widgets import (FieldContainer, FieldWidget, CollageIconRenderer, ThumbButton, ThumbRenderer, PanelWidget,
 							PanelModal, EditTextBox, EditTextLine, ProgressWidget, TagWidget, TagBoxWidget)
-from src.qt.modals import TagSearchPanel, BuildTagPanel, TagDatabasePanel
+from src.qt.modals import TagSearchPanel, BuildTagPanel, TagDatabasePanel, AddFieldModal
 import src.qt.resources_rc
 
 # SIGQUIT is not defined on Windows
@@ -732,74 +732,6 @@ class RelinkUnlinkedEntries(QObject):
 		# for d in deleted:
 		# 	self.lib.missing_files.remove(d)
 
-
-class AddFieldModal(QWidget):
-	done = Signal(int)
-	def __init__(self, library:'Library'):
-		# [Done]
-		# - OR -
-		# [Cancel] [Save]
-		super().__init__()
-		self.lib = library
-		self.setWindowTitle(f'Add Field')
-		self.setWindowModality(Qt.WindowModality.ApplicationModal)
-		self.setMinimumSize(400, 300)
-		self.root_layout = QVBoxLayout(self)
-		self.root_layout.setContentsMargins(6,6,6,6)
-
-		self.title_widget = QLabel()
-		self.title_widget.setObjectName('fieldTitle')
-		self.title_widget.setWordWrap(True)
-		self.title_widget.setStyleSheet(
-										# 'background:blue;'
-								 		# 'text-align:center;'
-										'font-weight:bold;'
-										'font-size:14px;'
-										'padding-top: 6px'
-										'')
-		self.title_widget.setText('Add Field')
-		self.title_widget.setAlignment(Qt.AlignmentFlag.AlignCenter)
-		
-		self.combo_box = QComboBox()
-		self.combo_box.setEditable(False)
-		# self.combo_box.setMaxVisibleItems(5)
-		self.combo_box.setStyleSheet('combobox-popup:0;')
-		self.combo_box.view().setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
-		for df in self.lib.default_fields:
-			self.combo_box.addItem(f'{df["name"]} ({df["type"].replace("_", " ").title()})')
-
-		self.button_container = QWidget()
-		self.button_layout = QHBoxLayout(self.button_container)
-		self.button_layout.setContentsMargins(6,6,6,6)
-		self.button_layout.addStretch(1)
-
-		# self.cancel_button = QPushButton()
-		# self.cancel_button.setText('Cancel')
-
-		self.cancel_button = QPushButton()
-		self.cancel_button.setText('Cancel')
-		self.cancel_button.clicked.connect(self.hide)
-		# self.cancel_button.clicked.connect(widget.reset)
-		self.button_layout.addWidget(self.cancel_button)
-	
-		self.save_button = QPushButton()
-		self.save_button.setText('Add')
-		# self.save_button.setAutoDefault(True)
-		self.save_button.setDefault(True)
-		self.save_button.clicked.connect(self.hide)
-		self.save_button.clicked.connect(lambda: self.done.emit(self.combo_box.currentIndex()))
-		# self.save_button.clicked.connect(lambda: save_callback(widget.get_content()))
-		self.button_layout.addWidget(self.save_button)
-
-		# self.returnPressed.connect(lambda: self.done.emit(self.combo_box.currentIndex()))
-		
-		# self.done.connect(lambda x: callback(x))
-		
-		self.root_layout.addWidget(self.title_widget)
-		self.root_layout.addWidget(self.combo_box)
-		# self.root_layout.setStretch(1,2)
-		self.root_layout.addStretch(1)
-		self.root_layout.addWidget(self.button_container)
 
 class FileExtensionModal(PanelWidget):
 	done = Signal()
