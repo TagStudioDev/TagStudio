d564354e0dd59b0b08caa8996fe935c0d47f25e8
DrRetro <colindrwho@gmail.com>
2024-04-30 20:09:10 -0400

    Fixes and Comments

--- tagstudio/src/qt/ts_qt.py
+++ tagstudio/src/qt/ts_qt.py
@@ -36,7 +36,7 @@ from PySide6.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, 
 							   QListView, QSplitter, QSizePolicy, QMessageBox, QBoxLayout, QCheckBox, QSplashScreen,
 							   QMenu, QTableWidget, QTableWidgetItem, QGraphicsView, QGraphicsScene, QGraphicsSceneMouseEvent)
 from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput, QMediaDevices
-from PySide6.QtMultimediaWidgets import QVideoWidget, QGraphicsVideoItem
+from PySide6.QtMultimediaWidgets import QGraphicsVideoItem
 from PySide6.QtSvgWidgets import QSvgWidget
 from humanfriendly import format_timespan, format_size
 
@@ -2776,37 +2776,48 @@ class PreviewPanel(QWidget):
 			callback()
 	
 class VideoPlayer(QGraphicsView):
+	"""A simple video player for the TagStudio application."""
 	resolution = QSize(1280, 720)
 	hover_fix_timer = QTimer()
-	play_pause = None
+	video_preview = None
+	play_pause = None 
+	mute_button = None 
 	content_visible = False
+	filepath = None
+
 	def __init__(self) -> None:
+		# Set up the base class.
 		super().__init__()
+		
 		self.animation = QVariantAnimation(self)
 		self.animation.valueChanged.connect(lambda value: self.set_tint_transparency(value))
 		
 		self.hover_fix_timer.timeout.connect(lambda: self.checkIfStillHovered())
 		self.hover_fix_timer.setSingleShot(True)
 
+		# Set up the video player.	
 		self.installEventFilter(self)
 		self.setMouseTracking(True)
 		self.setScene(QGraphicsScene(self))
 		self.setRenderHint(QPainter.RenderHint.SmoothPixmapTransform)
 		self.player = QMediaPlayer(self)
-		self.player.mediaStatusChanged.connect(lambda: logging.info(self.player.mediaStatus()))
+		self.player.mediaStatusChanged.connect(lambda: self.check_media_status(self.player.mediaStatus()))
 		self.video_preview = QGraphicsVideoItem()
 		self.player.setVideoOutput(self.video_preview)
 		self.video_preview.setAcceptHoverEvents(True)
 		self.video_preview.installEventFilter(self)
 		self.player.setAudioOutput(QAudioOutput(QMediaDevices().defaultAudioOutput(),self.player))
-
 		self.setAlignment(Qt.AlignmentFlag.AlignCenter)
 		self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 		self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
 		self.scene().addItem(self.video_preview)
 		self.setMouseTracking(True)
+
+		# Set up the video tint.
 		self.video_tint = self.scene().addRect(0, 0, self.video_preview.size().width(), self.video_preview.size().height(), QPen(QColor(0, 0, 0, 0)), QBrush(QColor(0, 0, 0, 0)))
 		# self.video_tint.setParentItem(self.video_preview)
+
+		# Set up the buttons.
 		self.play_pause = QSvgWidget('./tagstudio/resources/pause.svg', self)
 		self.play_pause.setMouseTracking(True)
 		self.play_pause.installEventFilter(self)
@@ -2814,6 +2825,7 @@ class VideoPlayer(QGraphicsView):
 		self.play_pause.resize(100, 100)
 		self.play_pause.move(self.width()/2 - self.play_pause.size().width()/2, self.height()/2 - self.play_pause.size().height()/2)
 		self.play_pause.hide()
+
 		self.mute_button = QSvgWidget('./tagstudio/resources/volume_muted.svg', self)
 		self.mute_button.setMouseTracking(True)
 		self.mute_button.installEventFilter(self)
@@ -2822,7 +2834,34 @@ class VideoPlayer(QGraphicsView):
 		self.mute_button.move(self.width() - self.mute_button.size().width()/2, self.height() - self.mute_button.size().height()/2)
 		self.mute_button.hide()
 
+	def check_media_status(self, media_status:QMediaPlayer.MediaStatus) -> None:
+		logging.info(media_status)
+		if media_status == QMediaPlayer.MediaStatus.EndOfMedia:
+			#Switches current video to with video at filepath. Reason for this is because Pyside6 is dumb and can't handle setting a new source and freezes.
+			#Even if I stop the player before switching, it breaks.
+			#On the plus side, this adds infinite looping for the video preview.
+			self.player.setSource(QUrl().fromLocalFile(self.filepath))
+			logging.info(f'Set source to {self.filepath}.')
+			# self.video_preview.setSize(self.resolution)
+			
+			self.player.setPosition(0)
+			logging.info(f'Set muted to true.')
+			self.player.play()
+			logging.info(f'Successfully played.')
+			self.keep_controls_in_place()
+			self.update_controls()
+	def update_controls(self) -> None:
+		if self.player.audioOutput().isMuted():
+			self.mute_button.load('./tagstudio/resources/volume_muted.svg')
+		else:
+			self.mute_button.load('./tagstudio/resources/volume_unmuted.svg')
+
+		if self.player.isPlaying():
+			self.play_pause.load('./tagstudio/resources/pause.svg')
+		else:
+			self.play_pause.load('./tagstudio/resources/play.svg')
 	def eventFilter(self, obj:QObject, event:QEvent) -> bool:
+		#This chunk of code is for the video controls.
 		if obj == self.play_pause and event.type() == QEvent.Type.MouseButtonPress and event.button() == Qt.MouseButton.LeftButton:
 			if self.player.hasVideo():
 				self.pause_toggle()
@@ -2844,6 +2883,8 @@ class VideoPlayer(QGraphicsView):
 		return super().eventFilter(obj, event)
 	
 	def checkIfStillHovered(self) -> None:
+		#Yet again, Pyside6 is dumb. I don't know why, but the HoverLeave event is not triggered sometimes and does not hide the controls.
+		#So, this is a workaround. This is called by a QTimer every 10ms to check if the mouse is still in the video preview.
 		if not self.video_preview.isUnderMouse():
 			self.releaseMouse()
 		else:
@@ -2860,6 +2901,7 @@ class VideoPlayer(QGraphicsView):
 		self.animation.start()
 		self.play_pause.show()
 		self.mute_button.show()
+		self.keep_controls_in_place()
 		return super().underMouse()
 
 	def releaseMouse(self) -> None:
@@ -2873,9 +2915,9 @@ class VideoPlayer(QGraphicsView):
 		return super().releaseMouse()
 	
 	def reset_controls_to_default(self) -> None:
+		# Resets the video controls to their default state.
 		self.play_pause.load('./tagstudio/resources/pause.svg')
 		self.mute_button.load('./tagstudio/resources/volume_muted.svg')
-		self.player.audioOutput().setMuted(True)
 
 
 	def pause_toggle(self) -> None:
@@ -2895,41 +2937,35 @@ class VideoPlayer(QGraphicsView):
 			self.mute_button.load('./tagstudio/resources/volume_muted.svg')
 
 	def play(self, filepath: str, resolution: QSize) -> None:
+		# Sets the filepath and sends the current player position to the very end, so that the new video can be played.
+		self.player.audioOutput().setMuted(True)
 		logging.info(f'Playing {filepath}')
+		self.resolution = resolution
+		self.filepath = filepath
 		if self.player.isPlaying():
 			self.player.setPosition(self.player.duration())
-			self.player.stop()
+			self.player.play()
+		else:
+			self.check_media_status(QMediaPlayer.MediaStatus.EndOfMedia)
+
 		logging.info(f'Successfully stopped.')
-		self.player.setSource(QUrl().fromLocalFile(filepath))
-		logging.info(f'Set source to {filepath}.')
-		self.resolution = resolution
-		# self.video_preview.setSize(self.resolution)
-		self.player.audioOutput().setMuted(True)
-		logging.info(f'Set muted to true.')
-		self.player.play()
-		logging.info(f'Successfully played.')
-		self.keep_controls_in_place()
-		self.reset_controls_to_default()
+
 	
 	def resize_video(self, new_size : QSize,) -> None:
+		# Resizes the video preview to the new size.
 		self.video_preview.setSize(new_size)
 		self.video_tint.setRect(0, 0, self.video_preview.size().width(), self.video_preview.size().height())
 		self.centerOn(self.video_preview)
 		self.keep_controls_in_place()
 
 	def keep_controls_in_place(self) -> None:
+		# Keeps the video controls in the places they should be.
 		self.play_pause.move(self.width()/2 - self.play_pause.size().width()/2, self.height()/2 - self.play_pause.size().height()/2)
 		self.mute_button.move(self.width() - self.mute_button.size().width()-10, self.height() - self.mute_button.size().height()-10)
 
 	def resizeEvent(self, event: QResizeEvent) -> None:
+		# Keeps the video preview in the center of the screen.
 		self.centerOn(self.video_preview)
-		return
-		# return super().resizeEvent(event)
-
-class VideoWidget(QWidget):
-
-
-	def resizeEvent(self, event: QResizeEvent) -> None:
 		return
 		# return super().resizeEvent(event)
 
