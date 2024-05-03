58c773a0dee48a2541e762ec44cb954c9a2bd517
JinguBangWest <lauren@sweetmoon.io>
2024-05-03 14:37:32 -0400

    change to string.removeprefix(prefix)

--- tagstudio/src/core/utils/web.py
+++ tagstudio/src/core/utils/web.py
@@ -6,6 +6,5 @@ def strip_web_protocol(string: str) -> str:
     """Strips a leading web protocol (ex. \"https://\") as well as \"www.\" from a string."""
     prefixes = ['https://','http://','www.','www2.']
     for prefix in prefixes:
-        if string.startswith(prefix):
-            string = string.replace(prefix, '')
+        string = string.removeprefix(prefix)
     return string
