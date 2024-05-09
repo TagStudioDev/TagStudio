44c6a969446f7d66a0566a37927bbfdcdc75ce79
Creepler13 <denis.schlichting03@gmail.com>
2024-05-10 00:47:46 +0200

    Added renaming option to drop import

--- tagstudio/src/qt/modals/drop_import.py
+++ tagstudio/src/qt/modals/drop_import.py
@@ -27,21 +27,40 @@ def dropEvent(driver: "QtDriver", event: QDropEvent):
                 )
                 msgBox.addButton("Skip", QMessageBox.ButtonRole.YesRole)
                 msgBox.addButton("Override", QMessageBox.ButtonRole.DestructiveRole)
+                msgBox.addButton("Rename", QMessageBox.ButtonRole.DestructiveRole)
                 msgBox.addButton("Cancel", QMessageBox.ButtonRole.NoRole)
                 ret = msgBox.exec()
 
-                if ret == 2:
+                if ret == 3:  # cancel
                     return
 
             for url in urls:
-                if ret == 0 and url in duplicate_filesnames:
-                    continue
+                filename = url.fileName()
+
+                if url in duplicate_filesnames:
+                    if ret == 0:  # skip duplicates
+                        continue
+                    if ret == 1:  # only override file contents of duplicates
+                        shutil.copyfile(
+                            url.toLocalFile(), driver.lib.library_dir + "/" + filename
+                        )
+                        continue
+                    if ret == 2:  # rename duplicates
+                        index = 2
+                        o_filename = url.fileName()
+                        dot_idx = o_filename.index(".")
+                        while os.path.exists(driver.lib.library_dir + "/" + filename):
+                            filename = (
+                                o_filename[:dot_idx]
+                                + f" ({index})"
+                                + o_filename[dot_idx:]
+                            )
+                            index += 1
+
                 shutil.copyfile(
-                    url.toLocalFile(), driver.lib.library_dir + "/" + url.fileName()
+                    url.toLocalFile(), driver.lib.library_dir + "/" + filename
                 )
-                if ret == 1 and url in duplicate_filesnames:
-                    continue
-                driver.lib.files_not_in_library.append(url.fileName())
+                driver.lib.files_not_in_library.append(filename)
 
             driver.add_new_files_runnable()
 
