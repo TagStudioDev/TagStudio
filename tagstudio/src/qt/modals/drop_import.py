95fe39392e75e6a2a41838e9053e4f396c413498
Creepler13 <denis.schlichting03@gmail.com>
2024-05-10 16:38:59 +0200

    Improved readability and switched to pathLib

--- tagstudio/src/qt/modals/drop_import.py
+++ tagstudio/src/qt/modals/drop_import.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import shutil
 import typing
 
@@ -10,59 +10,52 @@ if typing.TYPE_CHECKING:
 
 
 def dropEvent(driver: "QtDriver", event: QDropEvent):
-    if event.mimeData().hasUrls():
-        if event.mimeData().urls()[0].isLocalFile():
-            urls = event.mimeData().urls()
-            duplicate_filesnames = []
-            for url in urls:
-                if os.path.exists(driver.lib.library_dir + "/" + url.fileName()):
-                    duplicate_filesnames.append(url)
+    if not event.mimeData().hasUrls():  
+        return
+    
+    if not event.mimeData().urls()[0].isLocalFile():  
+        return  
+    
+    urls = event.mimeData().urls()
+    duplicate_filesnames = []
+    for url in urls:
+        if  Path(driver.lib.library_dir + "/" + url.fileName()).exists():
+            duplicate_filesnames.append(url)
 
-            ret = -1
+    ret = -1
 
-            if len(duplicate_filesnames) > 0:
-                msgBox = QMessageBox()
-                msgBox.setText(
-                    f"The files  {", ".join(map(lambda url:url.fileName(),duplicate_filesnames))}  have filenames that already exist in the library folder."
-                )
-                msgBox.addButton("Skip", QMessageBox.ButtonRole.YesRole)
-                msgBox.addButton("Override", QMessageBox.ButtonRole.DestructiveRole)
-                msgBox.addButton("Rename", QMessageBox.ButtonRole.DestructiveRole)
-                msgBox.addButton("Cancel", QMessageBox.ButtonRole.NoRole)
-                ret = msgBox.exec()
+    if len(duplicate_filesnames) > 0:
+        msgBox = QMessageBox()
+        msgBox.setText(
+            f"The files  {", ".join(map(lambda url:url.fileName(),duplicate_filesnames))}  have filenames that already exist in the library folder."
+        )
+        msgBox.addButton("Skip", QMessageBox.ButtonRole.YesRole)
+        msgBox.addButton("Override", QMessageBox.ButtonRole.DestructiveRole)
+        msgBox.addButton("Rename", QMessageBox.ButtonRole.DestructiveRole)
+        msgBox.addButton("Cancel", QMessageBox.ButtonRole.NoRole)
+        ret = msgBox.exec()
 
-                if ret == 3:  # cancel
-                    return
+        if ret == 3:  # cancel
+            return
 
-            for url in urls:
-                filename = url.fileName()
+    for url in urls:
+        filename = url.fileName()
 
-                if url in duplicate_filesnames:
-                    if ret == 0:  # skip duplicates
-                        continue
-                    if ret == 1:  # only override file contents of duplicates
-                        shutil.copyfile(
-                            url.toLocalFile(), driver.lib.library_dir + "/" + filename
-                        )
-                        continue
-                    if ret == 2:  # rename duplicates
-                        index = 2
-                        o_filename = url.fileName()
-                        dot_idx = o_filename.index(".")
-                        while os.path.exists(driver.lib.library_dir + "/" + filename):
-                            filename = (
-                                o_filename[:dot_idx]
-                                + f" ({index})"
-                                + o_filename[dot_idx:]
-                            )
-                            index += 1
-
-                shutil.copyfile(
-                    url.toLocalFile(), driver.lib.library_dir + "/" + filename
-                )
+        if url in duplicate_filesnames:
+            if ret == 0:  # skip duplicates
+                continue
+            
+            if ret == 2: # rename 
+                filename = get_renamed_duplicate_filename(driver.lib.library_dir,filename)
                 driver.lib.files_not_in_library.append(filename)
-
-            driver.add_new_files_runnable()
+        else: # override is simply copying but not adding a new entry
+            driver.lib.files_not_in_library.append(filename)
+            
+        shutil.copyfile(
+            url.toLocalFile(), driver.lib.library_dir + "/" + filename
+        )    
+            
+    driver.add_new_files_runnable()
 
 
 def dragEnterEvent(event: QDragEnterEvent):
@@ -77,3 +70,17 @@ def dragMoveEvent(event: QDragMoveEvent):
         event.accept()
     else:
         event.ignore()
+
+
+def get_renamed_duplicate_filename(path,filename)->str:
+        index = 2
+        o_filename = filename
+        dot_idx = o_filename.index(".")
+        while Path(path + "/" + filename).exists():
+            filename = (
+                o_filename[:dot_idx]
+                + f" ({index})"
+                + o_filename[dot_idx:]
+            )
+            index += 1
+        return filename    
