db8d94eb380863ef6ada7036cd684770e607c093
Creepler13 <denis.schlichting03@gmail.com>
2024-05-12 02:17:13 +0200

    Added support for folders

--- tagstudio/src/qt/modals/drop_import.py
+++ tagstudio/src/qt/modals/drop_import.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import shutil
 import typing
+import logging
 
 from PySide6.QtGui import QDropEvent, QDragEnterEvent, QDragMoveEvent
 from PySide6.QtWidgets import QMessageBox
@@ -8,54 +9,14 @@ from PySide6.QtWidgets import QMessageBox
 if typing.TYPE_CHECKING:
     from src.qt.ts_qt import QtDriver
 
+logging.basicConfig(format="[DROP IMPORT] %(message)s", level=logging.INFO)
+
 
 def dropEvent(driver: "QtDriver", event: QDropEvent):
     if not event.mimeData().hasUrls():
         return
 
-    if not event.mimeData().urls()[0].isLocalFile():
-        return
-
-    urls = event.mimeData().urls()
-    duplicate_filesnames = []
-    for url in urls:
-        if (Path(driver.lib.library_dir) / url.fileName()).exists():
-            duplicate_filesnames.append(url)
-
-    ret = -1
-
-    if len(duplicate_filesnames) > 0:
-        msgBox = QMessageBox()
-        msgBox.setText(
-            f"The files  {', '.join(map(lambda url:url.fileName(),duplicate_filesnames))}  have filenames that already exist in the library folder."
-        )
-        msgBox.addButton("Skip", QMessageBox.ButtonRole.YesRole)
-        msgBox.addButton("Override", QMessageBox.ButtonRole.DestructiveRole)
-        msgBox.addButton("Rename", QMessageBox.ButtonRole.DestructiveRole)
-        msgBox.addButton("Cancel", QMessageBox.ButtonRole.NoRole)
-        ret = msgBox.exec()
-
-        if ret == 3:  # cancel
-            return
-
-    for url in urls:
-        filename = url.fileName()
-
-        if url in duplicate_filesnames:
-            if ret == 0:  # skip duplicates
-                continue
-
-            if ret == 2:  # rename
-                filename = get_renamed_duplicate_filename(
-                    driver.lib.library_dir, filename
-                )
-                driver.lib.files_not_in_library.append(filename)
-        else:  # override is simply copying but not adding a new entry
-            driver.lib.files_not_in_library.append(filename)
-
-        shutil.copyfile(url.toLocalFile(), driver.lib.library_dir + "/" + filename)
-
-    driver.add_new_files_runnable()
+    import_files(driver, event.mimeData().urls())
 
 
 def dragEnterEvent(event: QDragEnterEvent):
@@ -72,11 +33,118 @@ def dragMoveEvent(event: QDragMoveEvent):
         event.ignore()
 
 
-def get_renamed_duplicate_filename(path, filename) -> str:
+def import_files(driver: "QtDriver", urls):
+    files: list[Path] = []
+    dirs_in_root: list[Path] = []
+    duplicate_files: list[Path] = []
+
+    for url in urls:
+        if url.isLocalFile():
+            file = Path(url.toLocalFile())
+
+            if file.is_dir():
+                files += get_files_in_folder(file)
+                dirs_in_root.append(file.parent)
+
+                dupes = get_files_exists_in_library(
+                    dirs_in_root, driver.lib.library_dir, file
+                )
+                duplicate_files += dupes
+            else:
+                files.append(file)
+
+                if file.parent not in dirs_in_root:
+                    dirs_in_root.append(
+                        file.parent
+                    )  # to create relative path of files not in folder
+
+                if (Path(driver.lib.library_dir) / file.name).exists():
+                    duplicate_files.append(file)
+
+    ret = -1
+
+    if len(duplicate_files) > 0:
+        msgBox = QMessageBox()
+        msgBox.setText(
+            f"The files  {', '.join(map(lambda path: str(path),get_relativ_paths(dirs_in_root,duplicate_files)))}  have filenames that already exist in the library folder."
+        )
+        msgBox.addButton("Skip", QMessageBox.ButtonRole.YesRole)
+        msgBox.addButton("Override", QMessageBox.ButtonRole.DestructiveRole)
+        msgBox.addButton("Rename", QMessageBox.ButtonRole.DestructiveRole)
+        msgBox.addButton("Cancel", QMessageBox.ButtonRole.NoRole)
+        ret = msgBox.exec()
+
+        if ret == 3:  # cancel
+            return
+    logging.info(files)
+    for file in files:
+        if file.is_dir():
+            continue
+
+        dest_file = get_relativ_path(dirs_in_root, file)
+
+        if file in duplicate_files:
+            if ret == 0:  # skip duplicates
+                continue
+
+            if ret == 2:  # rename
+                dest_file = dest_file.with_name(
+                    get_renamed_duplicate_filename(
+                        Path(driver.lib.library_dir), dest_file
+                    )
+                )
+                driver.lib.files_not_in_library.append(dest_file)
+        else:  # override is simply copying but not adding a new entry
+            driver.lib.files_not_in_library.append(dest_file)
+
+        (driver.lib.library_dir / dest_file).parent.mkdir(parents=True, exist_ok=True)
+        shutil.copyfile(file, driver.lib.library_dir / dest_file)
+
+    driver.add_new_files_runnable()
+
+
+def get_renamed_duplicate_filename(path: Path, filePath: Path) -> str:
     index = 2
-    o_filename = filename
+    o_filename = filePath.name
     dot_idx = o_filename.index(".")
-    while Path(path + "/" + filename).exists():
-        filename = o_filename[:dot_idx] + f" ({index})" + o_filename[dot_idx:]
+    while (path / filePath).exists():
+        filePath = filePath.with_name(
+            o_filename[:dot_idx] + f" ({index})" + o_filename[dot_idx:]
+        )
         index += 1
-    return filename
+    return filePath.name
+
+
+def get_files_in_folder(path: Path) -> list[Path]:
+    files = []
+    for file in path.glob("**/*"):
+        files.append(file)
+    return files
+
+
+def get_files_exists_in_library(
+    dirs_in_root: list[Path], lib_dir: str, path: Path
+) -> list[Path]:
+    exists: list[Path] = []
+    if path.is_dir():
+        files = get_files_in_folder(path)
+        for file in files:
+            if file.is_dir():
+                exists += get_files_exists_in_library(dirs_in_root, lib_dir, file)
+            elif (lib_dir / get_relativ_path(dirs_in_root, file)).exists():
+                exists.append(file)
+    return exists
+
+
+def get_relativ_paths(roots: list[Path], paths: list[Path]) -> list[Path]:
+    relativ_paths = []
+    for file in paths:
+        relativ_paths.append(get_relativ_path(roots, file))
+    return relativ_paths
+
+
+def get_relativ_path(roots: list[Path], path: Path) -> Path:
+    for dir in roots:
+        if path.is_relative_to(dir):
+            return path.relative_to(dir)
+    return path.name
