aa128f06898fe0a3537e60af6400c5317df213d1
Thesacraft <admin@samuelbellmann.de>
2024-05-13 15:52:17 +0200

    Refactors ThumbRenderer to be less repetitive

--- tagstudio/src/qt/helpers/__init__.py
+++ tagstudio/src/qt/helpers/__init__.py
@@ -1,3 +1,4 @@
 from .file_opener import open_file, FileOpenerHelper, FileOpenerLabel
 from .function_iterator import FunctionIterator
 from .custom_runnable import CustomRunnable
+from .gradient import four_corner_gradient_background
