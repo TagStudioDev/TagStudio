4ffe38b90fb4698317410dbf38477f4de776d0b1
Thesacraft <admin@samuelbellmann.de>
2024-05-13 16:55:46 +0200

    remove unused imports

--- tagstudio/src/qt/helpers/gradient.py
+++ tagstudio/src/qt/helpers/gradient.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2024 Travis Abendshien (CyanVoxel).
+# Licensed under the GPL-3.0 License.
+# Created for TagStudio: https://github.com/CyanVoxel/TagStudio
+
 from PIL import Image, ImageEnhance, ImageChops
 
 
