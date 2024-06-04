11f0c7f9b8003554dbfe14b640a788ee3a05a54b
PossiblePanda <85448494+PossiblePanda@users.noreply.github.com>
2024-06-04 17:13:57 -0400

    Added various file formats to constants.py (#231)
    * Added various file formats to constants.py
    
    * Update tagstudio/src/core/constants.py
    
    Co-authored-by: Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
    
    * Update tagstudio/src/core/constants.py
    
    Co-authored-by: Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>

--- tagstudio/src/core/constants.py
+++ tagstudio/src/core/constants.py
@@ -29,7 +29,16 @@ IMAGE_TYPES: list[str] = [
     ".j2k",
     ".jpg2",
 ]
-RAW_IMAGE_TYPES: list[str] = [".raw", ".dng", ".rw2", ".nef", ".arw", ".crw", ".cr3"]
+RAW_IMAGE_TYPES: list[str] = [
+    ".raw",
+    ".dng",
+    ".rw2",
+    ".nef",
+    ".arw",
+    ".crw",
+    ".cr2",
+    ".cr3",
+]
 VIDEO_TYPES: list[str] = [
     ".mp4",
     ".webm",
@@ -86,7 +95,16 @@ PLAINTEXT_TYPES: list[str] = [
 ]
 SPREADSHEET_TYPES: list[str] = [".csv", ".xls", ".xlsx", ".numbers", ".ods"]
 PRESENTATION_TYPES: list[str] = [".ppt", ".pptx", ".key", ".odp"]
-ARCHIVE_TYPES: list[str] = [".zip", ".rar", ".tar", ".tar", ".gz", ".tgz", ".7z"]
+ARCHIVE_TYPES: list[str] = [
+    ".zip",
+    ".rar",
+    ".tar",
+    ".tar",
+    ".gz",
+    ".tgz",
+    ".7z",
+    ".s7z",
+]
 PROGRAM_TYPES: list[str] = [".exe", ".app"]
 SHORTCUT_TYPES: list[str] = [".lnk", ".desktop", ".url"]
 
