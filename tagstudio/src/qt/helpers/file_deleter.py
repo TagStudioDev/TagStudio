eb182302895e554c902c4eeeaef3c6afd0c8dc9e
peterbousaada <peterbousaada@gmail.com>
2024-06-13 08:59:16 -0400

    Updated  to use pathlib instead of os

--- tagstudio/src/qt/helpers/file_deleter.py
+++ tagstudio/src/qt/helpers/file_deleter.py
@@ -1,5 +1,5 @@
 import logging
-import os.path
+import pathlib
 import subprocess
 import sys
 import traceback
@@ -15,12 +15,13 @@ logging.basicConfig(format="%(message)s", level=logging.INFO)
 
 def delete_file(path: str | Path, callback: Callable):
     _path = str(path)
+    _file = Path(_path)
     logging.info(f"Deleting file: {_path}")
-    if not os.path.exists(_path):
+    if not _file.exists():
         logging.error(f"File not found: {_path}")
         return
     try:
-        os.remove(path)
+        _file.unlink()
         callback()
     except:
         traceback.print_exc()
