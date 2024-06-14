e31f75c1c6d8db59abe9ebc825b5605cba55cb74
Hayden Andreyka <haydenandreyka@gmail.com>
2024-06-13 17:18:02 -0700

    fix: python complaining about backslashes inside f-string expressions refactor excessively long f-string into separate variables

--- tagstudio/src/qt/modals/drop_import.py
+++ tagstudio/src/qt/modals/drop_import.py
@@ -167,10 +167,11 @@ class DropImport:
         dupes_to_show = self.duplicate_files
         if len(self.duplicate_files) > display_limit:
             dupes_to_show = dupes_to_show[0:display_limit]
+        
+        dupes_str = '\n    '.join(map(lambda path: str(path),dupes_to_show))
+        dupes_more = "\nand {len(self.duplicate_files)-display_limit} more " if len(self.duplicate_files) > display_limit else '\n'
 
-        msgBox.setText(
-            f"The following files:\n    {'\n    '.join(map(lambda path: str(path),self.get_relative_paths(dupes_to_show)))} {(f'\nand {len(self.duplicate_files)-display_limit} more ') if len(self.duplicate_files)>display_limit else '\n'}have filenames that already exist in the library folder."
-        )
+        msgBox.setText(f"The following files:\n    {dupes_str}{dupes_more}have filenames that already exist in the library folder.")
         msgBox.addButton("Skip", QMessageBox.ButtonRole.YesRole)
         msgBox.addButton("Override", QMessageBox.ButtonRole.DestructiveRole)
         msgBox.addButton("Rename", QMessageBox.ButtonRole.DestructiveRole)
