9cc62f73e8de13c0a03445210cd1f3f87ae1ca45
Brady <brady@latessa.net>
2024-07-16 22:57:52 -0400

    added ability to re-assign current tag

--- tagstudio/src/qt/modals/merge_tag.py
+++ tagstudio/src/qt/modals/merge_tag.py
@@ -1,5 +1,7 @@
 from PySide6.QtWidgets import QDialog, QVBoxLayout, QLabel, QComboBox, QPushButton
 from PySide6.QtCore import Qt
+import logging
+
 
 class MergeTagModal(QDialog):
     def __init__(self, library, current_tag):
@@ -10,23 +12,22 @@ class MergeTagModal(QDialog):
 
     def init_ui(self):
         layout = QVBoxLayout()
-        
+
         layout.addWidget(QLabel("Selected tag:"))
         self.selected_tag_dropdown = QComboBox(self)
         for tag in self.lib.tags:
             self.selected_tag_dropdown.addItem(tag.display_name(self.lib), tag)
         self.selected_tag_dropdown.setCurrentIndex(self.find_current_tag_index())
+        self.selected_tag_dropdown.currentIndexChanged.connect(self.update_current_tag)
         layout.addWidget(self.selected_tag_dropdown)
-        
+
         arrow_label = QLabel("↓")
         arrow_label.setAlignment(Qt.AlignCenter)
         layout.addWidget(arrow_label)
 
         layout.addWidget(QLabel("Select tag to merge with:"))
         self.tag2_dropdown = QComboBox(self)
-        for tag in self.lib.tags:
-            if tag.id != self.current_tag.id:
-                self.tag2_dropdown.addItem(tag.display_name(self.lib), tag)
+        self.update_tag2_dropdown()
         layout.addWidget(self.tag2_dropdown)
 
         self.merge_button = QPushButton("Merge", self)
@@ -42,6 +43,21 @@ class MergeTagModal(QDialog):
                 return index
         return 0
 
+    def update_current_tag(self):
+        self.current_tag = self.selected_tag_dropdown.currentData()
+        self.update_tag2_dropdown()
+
+    def update_tag2_dropdown(self):
+        self.tag2_dropdown.clear()
+        for tag in self.lib.tags:
+            if tag.id != self.current_tag.id:
+                self.tag2_dropdown.addItem(tag.display_name(self.lib), tag)
+
     def merge_tags(self):
-        self.lib.merge_tag(self.current_tag, self.tag2_dropdown.currentData())
-        self.accept()
+        target_tag = self.tag2_dropdown.currentData()
+        if target_tag and self.current_tag != target_tag:
+            self.lib.merge_tag(self.current_tag, target_tag)
+            self.accept()
+        else:
+            logging.error("MergeTagModal: Invalid tag selection.")
+            self.reject()
