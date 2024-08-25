a28305b84348459585415651fcdefd4ab895c407
HEROgold <martijnwieringa28@gmail.com>
2024-08-25 03:48:22 +0200

    use handlers

--- tagstudio/logger.py
+++ tagstudio/logger.py
@@ -5,6 +5,8 @@ from args import TagStudioArgs, parser
 args = parser.parse_args(namespace=TagStudioArgs)
 
 tag_studio_log = logging.getLogger(__name__)
+tag_studio_log.addHandler(logging.FileHandler("tagstudio.log"))
+tag_studio_log.addHandler(logging.StreamHandler()) # print logs.
 
 if args.debug:
     tag_studio_log.setLevel(logging.DEBUG)
