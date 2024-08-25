f49d712eefb2fb8fcb9330f7959e423be7179a67
HEROgold <martijnwieringa28@gmail.com>
2024-08-25 03:31:34 +0200

    move out args, for easier access everywhere.

---
+++ tagstudio/args.py
@@ -0,0 +1,41 @@
+# Parse arguments.
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-o",
+    "--open",
+    dest="open",
+    type=str,
+    help="Path to a TagStudio Library folder to open on start.",
+)
+parser.add_argument(
+    "-c",
+    "--config-file",
+    dest="config_file",
+    type=str,
+    help="Path to a TagStudio .ini or .plist config file to use.",
+)
+
+# parser.add_argument('--browse', dest='browse', action='store_true',
+#                     help='Jumps to entry browsing on startup.')
+# parser.add_argument('--external_preview', dest='external_preview', action='store_true',
+#                     help='Outputs current preview thumbnail to a live-updating file.')
+parser.add_argument(
+    "--debug",
+    dest="debug",
+    action="store_true",
+    help="Reveals additional internal data useful for debugging.",
+)
+parser.add_argument(
+    "--ui",
+    dest="ui",
+    type=str,
+    help="User interface option for TagStudio. Options: qt, cli (Default: qt)",
+)
+parser.add_argument(
+    "--ci",
+    action=argparse.BooleanOptionalAction,
+    help="Exit the application after checking it starts without any problem. Meant for CI check.",
+)
