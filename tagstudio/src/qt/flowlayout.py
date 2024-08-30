feb08cd5ac7a09b74aa796b9b8615f9e949d0c7b
ioorik <yotammaylat@gmail.com>
2024-08-30 16:14:40 +0300

    flowlayout and main file(tag_studio.py) documentation

--- tagstudio/src/qt/flowlayout.py
+++ tagstudio/src/qt/flowlayout.py
@@ -23,101 +23,149 @@ from PySide6.QtWidgets import QLayout, QSizePolicy, QWidget
 
 
 class FlowWidget(QWidget):
+    """
+    From what I understand:
+    A generic Qt window.
+    Please fix  my description
+    """
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
         self.ignore_size: bool = False
 
 
 class FlowLayout(QLayout):
-    def __init__(self, parent=None):
+    """A generic Qt layout + the ability to keep track of the items in the layout."""
+    def __init__(self, parent=None) -> None:
+        # Initialize the parent class
         super().__init__(parent)
 
+        # In case parent is set
         if parent is not None:
+            # Set margins to 0.
             self.setContentsMargins(QMargins(0, 0, 0, 0))
 
-        self._item_list = []
-        self.grid_efficiency = False
+        # Initialize the items list to nothing.
+        self._item_list: list = []
+        # Initialize grid efficiency to "False"
+        self.grid_efficiency: bool = False
 
     def __del__(self):
         item = self.takeAt(0)
         while item:
             item = self.takeAt(0)
 
-    def addItem(self, item):
+    def addItem(self, item) -> None:
+        """Add a given item to the list of items inside the layout."""
         self._item_list.append(item)
 
-    def count(self):
+    def count(self) -> int:
+        """Returns the amount of items in the layout"""
         return len(self._item_list)
 
-    def itemAt(self, index):
-        if 0 <= index < len(self._item_list):
+    def itemAt(self, index: int):
+        """Returns the item at a given index or False if the index is out of range."""
+        # Try to get the value at index "index" (supports negative numbers).
+        try:
             return self._item_list[index]
+        except IndexError:
+            return False
 
-        return None
-
-    def takeAt(self, index):
-        if 0 <= index < len(self._item_list):
+    def takeAt(self, index: int):
+        """
+        Acts like the pop instructions of a list.
+        (Because that's exactly what it does but don't tell anyone.)
+        """
+        # Try to pop the value at index "index" (supports negative numbers).
+        try:
             return self._item_list.pop(index)
-
-        return None
+        except IndexError:
+            return False
 
     def expandingDirections(self):
         return Qt.Orientation(0)
 
-    def hasHeightForWidth(self):
+    def hasHeightForWidth(self) -> True:
+        # @Travis please explain this?
         return True
 
-    def heightForWidth(self, width):
-        height = self._do_layout(QRect(0, 0, width, 0), True)
+    def heightForWidth(self, width) -> int | float:
+        height: int | float = self._do_layout(QRect(0, 0, width, 0), True)
         return height
 
     def setGeometry(self, rect):
+        """Sets size and position of the layout"""
         super(FlowLayout, self).setGeometry(rect)
         self._do_layout(rect, False)
 
-    def setGridEfficiency(self, bool):
+    def setGridEfficiency(self, state: bool) -> None:
         """
         Enables or Disables efficiencies when all objects are equally sized.
         """
-        self.grid_efficiency = bool
+        self.grid_efficiency = state
 
-    def sizeHint(self):
+    def sizeHint(self) -> QSize:
         return self.minimumSize()
 
-    def minimumSize(self):
+    def minimumSize(self) -> QSize:
+        """Calculates the minimum size of the layout based on the items and grid efficiency mode?"""
+        # Check if grid efficiency is turned on.
         if self.grid_efficiency:
+            # In case it is: Check if the layout's items list is *not* empty
             if self._item_list:
+                # If it is not empty: Return the first object of the items list.
                 return self._item_list[0].minimumSize()
             else:
+                # If it is empty: Return?
                 return QSize()
+        # If grid efficiency is not on.
         else:
+            # Initialize a size variable.
             size = QSize()
 
+            # Per item in the items list
             for item in self._item_list:
+                # Expand the size of the initial size variable
+                # (Sets the size to the combined size of all the items in the list)
                 size = size.expandedTo(item.minimumSize())
 
             size += QSize(
                 2 * self.contentsMargins().top(), 2 * self.contentsMargins().top()
             )
+
+            # Return the final size.
             return size
 
-    def _do_layout(self, rect, test_only):
-        x = rect.x()
-        y = rect.y()
-        line_height = 0
-        spacing = self.spacing()
+    def _do_layout(self, rect, test_only) -> int | float:
+
+        #
+        # Initialize variables.
+        #
+        x: int | float = rect.x()
+        y: int | float = rect.y()
+        line_height: int | float = 0
+        spacing: int = self.spacing()
+        # Declare for later use.
         item = None
         style = None
         layout_spacing_x = None
         layout_spacing_y = None
+        #
+        # End init.
+        #
 
+        # Check if the grid efficiency mode is on.
         if self.grid_efficiency:
+            # If there are items in the list.
             if self._item_list:
+                # Set the item variable to the first item in the list
                 item = self._item_list[0]
+                # Set the style variable to the style of the item's widget
                 style = item.widget().style()
+                # Set the x spacing
                 layout_spacing_x = style.layoutSpacing(
                     QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal
                 )
+                # Set the y spacing
                 layout_spacing_y = style.layoutSpacing(
                     QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical
                 )
--- tagstudio/tag_studio.py
+++ tagstudio/tag_studio.py
@@ -11,12 +11,13 @@ import argparse
 import traceback
 
 
-def main():
+def main() -> None:
     # appid = "cyanvoxel.tagstudio.9"
     # ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(appid)
 
     # Parse arguments.
-    parser = argparse.ArgumentParser()
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    # Add an "open" argument as a library to open while starting.
     parser.add_argument(
         "-o",
         "--open",
@@ -24,6 +25,7 @@ def main():
         type=str,
         help="Path to a TagStudio Library folder to open on start.",
     )
+    # Add "config file"(-c) option to use as the config of the program.
     parser.add_argument(
         "-c",
         "--config-file",
@@ -36,46 +38,67 @@ def main():
     #                     help='Jumps to entry browsing on startup.')
     # parser.add_argument('--external_preview', dest='external_preview', action='store_true',
     #                     help='Outputs current preview thumbnail to a live-updating file.')
+
+    # Add "debug" argument to help debug the program.
     parser.add_argument(
         "--debug",
         dest="debug",
         action="store_true",
         help="Reveals additional internal data useful for debugging.",
     )
+    # Add "ui" argument to determine the library to use?
     parser.add_argument(
         "--ui",
         dest="ui",
         type=str,
         help="User interface option for TagStudio. Options: qt, cli (Default: qt)",
     )
+    # Add a "ci" argument to exit after successfully start-up
     parser.add_argument(
         "--ci",
         action=argparse.BooleanOptionalAction,
         help="Exit the application after checking it starts without any problem. Meant for CI check.",
     )
-    args = parser.parse_args()
+    # Parse all of the arguments
+    args: argparse.Namespace = parser.parse_args()
 
-    core = TagStudioCore()  # The TagStudio Core instance. UI agnostic.
+    core: TagStudioCore = TagStudioCore()  # The TagStudio Core instance. UI agnostic.
+    # TODO: Check if this is necessary:
     driver = None  # The UI driver instance.
     ui_name: str = "unknown"  # Display name for the UI, used in logs.
 
+    #
     # Driver selection based on parameters.
+    #
+    #In case the driver is a Qt driver
     if args.ui and args.ui == "qt":
+        # Run the TagStudio Qt driver
         driver = QtDriver(core, args)
+        # Save UI name for later use(crash message)
         ui_name = "Qt"
+    # In case the driver is a CLI(command line interface) driver
     elif args.ui and args.ui == "cli":
+        # Run the TagStudio CLI driver
         driver = CliDriver(core, args)
+        # Save UI name for later use(crash message)
         ui_name = "CLI"
+    # In case UI was not specified
     else:
+        # Run the TagStudio Qt driver by default
         driver = QtDriver(core, args)
+        # Save UI name for later use(crash message)
         ui_name = "Qt"
 
-    # Run the chosen frontend driver.
+    # Try to run the chosen frontend driver.
     try:
         driver.start()
+    # TODO: Potentially remove the try except since "driver.start" has it covered?
+    # In case of failure
     except Exception:
         traceback.print_exc()
+        # Display a crash message.
         print(f"\nTagStudio Frontend ({ui_name}) Crashed! Press Enter to Continue...")
+        # Wait for user input.
         input()
 
 
