9e2d1b4d55488022f521e15cd224863dba2d4cc0
Travis Abendshien <lvnvtravis@gmail.com>
2024-10-12 15:59:58 -0700

    tests: add test comparing svg to png snapshot
    Co-Authored-By: yed <yedpodtrzitko@users.noreply.github.com>

---
+++ tagstudio/tests/qt/test_thumb_renderer.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2024 Travis Abendshien (CyanVoxel).
+# Licensed under the GPL-3.0 License.
+# Created for TagStudio: https://github.com/CyanVoxel/TagStudio
+
+import io
+from pathlib import Path
+
+from PIL import Image
+from src.qt.widgets.thumb_renderer import ThumbRenderer
+from syrupy.extensions.image import PNGImageSnapshotExtension
+
+
+def test_svg_preview(cwd, snapshot):
+    file_path: Path = cwd / "fixtures" / "sample.svg"
+    renderer = ThumbRenderer()
+    img: Image.Image = renderer._image_vector_thumb(file_path, 200)
+
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format="PNG")
+    img_bytes.seek(0)
+    assert img_bytes.read() == snapshot(extension_class=PNGImageSnapshotExtension)
