823f1bb7964ff4750acdce7a3c8810a0f9880848
Travis Abendshien <lvnvtravis@gmail.com>
2024-10-12 14:32:48 -0700

    tests: compare epub cover against png snapshot
    Co-Authored-By: yed <yedpodtrzitko@users.noreply.github.com>

--- tagstudio/tests/qt/test_thumb_renderer.py
+++ tagstudio/tests/qt/test_thumb_renderer.py
@@ -2,18 +2,24 @@
 # Licensed under the GPL-3.0 License.
 # Created for TagStudio: https://github.com/CyanVoxel/TagStudio
 
+import io
 from pathlib import Path
 
 from PIL import Image
 from src.qt.widgets.thumb_renderer import ThumbRenderer
+from syrupy.extensions.image import PNGImageSnapshotExtension
 
 
-def test_epub_to_png():
+def test_epub_preview(cwd, snapshot):
     # sample.epub, aka "Garrish, Matt. Epub 3 Best Practices (Excerpt). O’Reilly, 2013."
     # provided by https://idpf.github.io/epub3-samples/30/samples.html
     # under the CC-BY-SA 3.0 license: https://creativecommons.org/licenses/by-sa/3.0/
-    file_path = Path(__file__).parents[1] / "fixtures" / "sample.epub"
+    file_path: Path = cwd / "fixtures" / "sample.epub"
     tr = ThumbRenderer()
-    cover_image: Image.Image = tr._epub_cover(file_path)
+    img: Image.Image = tr._epub_cover(file_path)
 
-    assert cover_image is not None
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format="PNG")
+    img_bytes.seek(0)
+
+    assert img_bytes.read() == snapshot(extension_class=PNGImageSnapshotExtension)
