32641bdc502bf92a2f54f408d0a4cf04d768f68b
Travis Abendshien <lvnvtravis@gmail.com>
2024-10-12 15:33:42 -0700

    tests: add test comparing pdf to png snapshot
    Co-Authored-By: yed <yedpodtrzitko@users.noreply.github.com>

---
+++ tagstudio/tests/qt/test_thumb_renderer.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2024 Travis Abendshien (CyanVoxel).
+# Licensed under the GPL-3.0 License.
+# Created for TagStudio: https://github.com/CyanVoxel/TagStudio
+
+import io
+from pathlib import Path
+
+from PIL import Image
+from src.qt.widgets.thumb_renderer import ThumbRenderer
+from syrupy.extensions.image import PNGImageSnapshotExtension
+
+
+def test_pdf_preview(cwd, snapshot):
+    file_path: Path = cwd / "fixtures" / "sample.pdf"
+    renderer = ThumbRenderer()
+    img: Image.Image = renderer._pdf_thumb(file_path, 200)
+    img.save("sample.png", "PNG")
+
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format="PNG")
+    img_bytes.seek(0)
+    assert img_bytes.read() == snapshot(extension_class=PNGImageSnapshotExtension)
