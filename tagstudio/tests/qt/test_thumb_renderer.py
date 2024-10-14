3d7629bc731286d2721f19cc60bb8bb42e6facf5
Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
2024-10-14 16:34:49 -0700

    feat: add pdf thumbnail support (port #378) (#543)
    * feat: add pdf thumbnail support
    
    Co-Authored-By: Heiholf <71659566+heiholf@users.noreply.github.com>
    
    * fix: remove redef
    
    * tests: add test comparing pdf to png snapshot
    
    Co-Authored-By: yed <yedpodtrzitko@users.noreply.github.com>
    
    * fix: fix info in docstrings
    
    * fix: remove sample png generation
    
    * fix: change the pdf snapshot to use a black square
    
    * chore: fix whitespace
    
    ---------
    
    Co-authored-by: Heiholf <71659566+heiholf@users.noreply.github.com>
    Co-authored-by: yed <yedpodtrzitko@users.noreply.github.com>

--- tagstudio/tests/qt/test_thumb_renderer.py
+++ tagstudio/tests/qt/test_thumb_renderer.py
@@ -10,6 +10,17 @@ from src.qt.widgets.thumb_renderer import ThumbRenderer
 from syrupy.extensions.image import PNGImageSnapshotExtension
 
 
+def test_pdf_preview(cwd, snapshot):
+    file_path: Path = cwd / "fixtures" / "sample.pdf"
+    renderer = ThumbRenderer()
+    img: Image.Image = renderer._pdf_thumb(file_path, 200)
+
+    img_bytes = io.BytesIO()
+    img.save(img_bytes, format="PNG")
+    img_bytes.seek(0)
+    assert img_bytes.read() == snapshot(extension_class=PNGImageSnapshotExtension)
+
+
 def test_svg_preview(cwd, snapshot):
     file_path: Path = cwd / "fixtures" / "sample.svg"
     renderer = ThumbRenderer()
