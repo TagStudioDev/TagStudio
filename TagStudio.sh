54378debb8aa24916a2a944533cf118e1c49578e
Gred <Lolka200425@yandex.ru>
2024-11-01 11:24:37 +0300

    Change start files

--- TagStudio.sh
+++ TagStudio.sh
@@ -4,4 +4,5 @@ cd "$(dirname "$0")"
 ! [ -d .venv ] && python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-python tagstudio/tag_studio.py
+cd tagstudio
+python tag_studio.py
