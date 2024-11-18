4d3fc83c9b933821992737d2ca325f830c8270fb
Coolio <softwaredevelopment.stingray177@passinbox.com>
2024-11-18 14:21:30 -0600

    Updated as per disscussion in issue #591 (DRAFT

--- tagstudio/src/qt/modals/build_tag.py
+++ tagstudio/src/qt/modals/build_tag.py
@@ -27,194 +27,203 @@ logger = structlog.get_logger(__name__)
 
 
 class BuildTagPanel(PanelWidget):
-    on_edit = Signal(Tag)
+        on_edit = Signal(Tag)
+        disable_save = Signal()
+        enable_save = Signal()
 
-    def __init__(self, library: Library, tag: Tag | None = None):
-        super().__init__()
-        self.lib = library
-        # self.callback = callback
-        # self.tag_id = tag_id
+        def __init__(self, library: Library, tag: Tag | None = None):
+            super().__init__()
+            self.lib = library
+            # self.callback = callback
+            # self.tag_id = tag_id
 
-        self.setMinimumSize(300, 400)
-        self.root_layout = QVBoxLayout(self)
-        self.root_layout.setContentsMargins(6, 0, 6, 0)
-        self.root_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+            self.setMinimumSize(300, 400)
+            self.root_layout = QVBoxLayout(self)
+            self.root_layout.setContentsMargins(6, 0, 6, 0)
+            self.root_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
-        # Name -----------------------------------------------------------------
-        self.name_widget = QWidget()
-        self.name_layout = QVBoxLayout(self.name_widget)
-        self.name_layout.setStretch(1, 1)
-        self.name_layout.setContentsMargins(0, 0, 0, 0)
-        self.name_layout.setSpacing(0)
-        self.name_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self.name_title = QLabel()
-        self.name_title.setText("Name")
-        self.name_layout.addWidget(self.name_title)
-        self.name_field = QLineEdit()
-        self.name_field.setPlaceholderText("New Tag")
-        self.name_layout.addWidget(self.name_field)
+            # Name -----------------------------------------------------------------
+            self.name_widget = QWidget()
+            self.name_layout = QVBoxLayout(self.name_widget)
+            self.name_layout.setStretch(1, 1)
+            self.name_layout.setContentsMargins(0, 0, 0, 0)
+            self.name_layout.setSpacing(0)
+            self.name_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+            self.name_title = QLabel()
+            self.name_title.setText("Name")
+            self.name_layout.addWidget(self.name_title)
+            self.name_field = QLineEdit()
+            self.name_field.textChanged.connect(self.on_name_changed)
 
-        # Shorthand ------------------------------------------------------------
-        self.shorthand_widget = QWidget()
-        self.shorthand_layout = QVBoxLayout(self.shorthand_widget)
-        self.shorthand_layout.setStretch(1, 1)
-        self.shorthand_layout.setContentsMargins(0, 0, 0, 0)
-        self.shorthand_layout.setSpacing(0)
-        self.shorthand_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self.shorthand_title = QLabel()
-        self.shorthand_title.setText("Shorthand")
-        self.shorthand_layout.addWidget(self.shorthand_title)
-        self.shorthand_field = QLineEdit()
-        self.shorthand_layout.addWidget(self.shorthand_field)
+            self.name_layout.addWidget(self.name_field)
 
-        # Aliases --------------------------------------------------------------
-        self.aliases_widget = QWidget()
-        self.aliases_layout = QVBoxLayout(self.aliases_widget)
-        self.aliases_layout.setStretch(1, 1)
-        self.aliases_layout.setContentsMargins(0, 0, 0, 0)
-        self.aliases_layout.setSpacing(0)
-        self.aliases_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self.aliases_title = QLabel()
-        self.aliases_title.setText("Aliases")
-        self.aliases_layout.addWidget(self.aliases_title)
-        self.aliases_field = QTextEdit()
-        self.aliases_field.setAcceptRichText(False)
-        self.aliases_field.setMinimumHeight(40)
-        self.aliases_layout.addWidget(self.aliases_field)
+            # Shorthand ------------------------------------------------------------
+            self.shorthand_widget = QWidget()
+            self.shorthand_layout = QVBoxLayout(self.shorthand_widget)
+            self.shorthand_layout.setStretch(1, 1)
+            self.shorthand_layout.setContentsMargins(0, 0, 0, 0)
+            self.shorthand_layout.setSpacing(0)
+            self.shorthand_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+            self.shorthand_title = QLabel()
+            self.shorthand_title.setText("Shorthand")
+            self.shorthand_layout.addWidget(self.shorthand_title)
+            self.shorthand_field = QLineEdit()
+            self.shorthand_layout.addWidget(self.shorthand_field)
 
-        # Subtags ------------------------------------------------------------
-        self.subtags_widget = QWidget()
-        self.subtags_layout = QVBoxLayout(self.subtags_widget)
-        self.subtags_layout.setStretch(1, 1)
-        self.subtags_layout.setContentsMargins(0, 0, 0, 0)
-        self.subtags_layout.setSpacing(0)
-        self.subtags_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+            # Aliases --------------------------------------------------------------
+            self.aliases_widget = QWidget()
+            self.aliases_layout = QVBoxLayout(self.aliases_widget)
+            self.aliases_layout.setStretch(1, 1)
+            self.aliases_layout.setContentsMargins(0, 0, 0, 0)
+            self.aliases_layout.setSpacing(0)
+            self.aliases_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+            self.aliases_title = QLabel()
+            self.aliases_title.setText("Aliases")
+            self.aliases_layout.addWidget(self.aliases_title)
+            self.aliases_field = QTextEdit()
+            self.aliases_field.setAcceptRichText(False)
+            self.aliases_field.setMinimumHeight(40)
+            self.aliases_layout.addWidget(self.aliases_field)
 
-        self.subtags_title = QLabel()
-        self.subtags_title.setText("Parent Tags")
-        self.subtags_layout.addWidget(self.subtags_title)
+            # Subtags ------------------------------------------------------------
+            self.subtags_widget = QWidget()
+            self.subtags_layout = QVBoxLayout(self.subtags_widget)
+            self.subtags_layout.setStretch(1, 1)
+            self.subtags_layout.setContentsMargins(0, 0, 0, 0)
+            self.subtags_layout.setSpacing(0)
+            self.subtags_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
-        self.scroll_contents = QWidget()
-        self.scroll_layout = QVBoxLayout(self.scroll_contents)
-        self.scroll_layout.setContentsMargins(6, 0, 6, 0)
-        self.scroll_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+            self.subtags_title = QLabel()
+            self.subtags_title.setText("Parent Tags")
+            self.subtags_layout.addWidget(self.subtags_title)
 
-        self.scroll_area = QScrollArea()
-        # self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
-        self.scroll_area.setWidgetResizable(True)
-        self.scroll_area.setFrameShadow(QFrame.Shadow.Plain)
-        self.scroll_area.setFrameShape(QFrame.Shape.NoFrame)
-        self.scroll_area.setWidget(self.scroll_contents)
-        # self.scroll_area.setMinimumHeight(60)
+            self.scroll_contents = QWidget()
+            self.scroll_layout = QVBoxLayout(self.scroll_contents)
+            self.scroll_layout.setContentsMargins(6, 0, 6, 0)
+            self.scroll_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
-        self.subtags_layout.addWidget(self.scroll_area)
+            self.scroll_area = QScrollArea()
+            # self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOn)
+            self.scroll_area.setWidgetResizable(True)
+            self.scroll_area.setFrameShadow(QFrame.Shadow.Plain)
+            self.scroll_area.setFrameShape(QFrame.Shape.NoFrame)
+            self.scroll_area.setWidget(self.scroll_contents)
+            # self.scroll_area.setMinimumHeight(60)
 
-        self.subtags_add_button = QPushButton()
-        self.subtags_add_button.setText("+")
-        tsp = TagSearchPanel(self.lib)
-        tsp.tag_chosen.connect(lambda x: self.add_subtag_callback(x))
-        self.add_tag_modal = PanelModal(tsp, "Add Parent Tags", "Add Parent Tags")
-        self.subtags_add_button.clicked.connect(self.add_tag_modal.show)
-        self.subtags_layout.addWidget(self.subtags_add_button)
+            self.subtags_layout.addWidget(self.scroll_area)
 
-        # self.subtags_field = TagBoxWidget()
-        # self.subtags_field.setMinimumHeight(60)
-        # self.subtags_layout.addWidget(self.subtags_field)
+            self.subtags_add_button = QPushButton()
+            self.subtags_add_button.setText("+")
+            tsp = TagSearchPanel(self.lib)
+            tsp.tag_chosen.connect(lambda x: self.add_subtag_callback(x))
+            self.add_tag_modal = PanelModal(tsp, "Add Parent Tags", "Add Parent Tags")
+            self.subtags_add_button.clicked.connect(self.add_tag_modal.show)
+            self.subtags_layout.addWidget(self.subtags_add_button)
 
-        # Shorthand ------------------------------------------------------------
-        self.color_widget = QWidget()
-        self.color_layout = QVBoxLayout(self.color_widget)
-        self.color_layout.setStretch(1, 1)
-        self.color_layout.setContentsMargins(0, 0, 0, 0)
-        self.color_layout.setSpacing(0)
-        self.color_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self.color_title = QLabel()
-        self.color_title.setText("Color")
-        self.color_layout.addWidget(self.color_title)
-        self.color_field = QComboBox()
-        self.color_field.setEditable(False)
-        self.color_field.setMaxVisibleItems(10)
-        self.color_field.setStyleSheet("combobox-popup:0;")
-        for color in TagColor:
-            self.color_field.addItem(color.name, userData=color.value)
-        # self.color_field.setProperty("appearance", "flat")
-        self.color_field.currentIndexChanged.connect(
-            lambda c: (
-                self.color_field.setStyleSheet(
-                    "combobox-popup:0;"
-                    "font-weight:600;"
-                    f"color:{get_tag_color(ColorType.TEXT, self.color_field.currentData())};"
-                    f"background-color:{get_tag_color(
-                        ColorType.PRIMARY, 
-                        self.color_field.currentData())};"
+            # self.subtags_field = TagBoxWidget()
+            # self.subtags_field.setMinimumHeight(60)
+            # self.subtags_layout.addWidget(self.subtags_field)
+
+            # Shorthand ------------------------------------------------------------
+            self.color_widget = QWidget()
+            self.color_layout = QVBoxLayout(self.color_widget)
+            self.color_layout.setStretch(1, 1)
+            self.color_layout.setContentsMargins(0, 0, 0, 0)
+            self.color_layout.setSpacing(0)
+            self.color_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+            self.color_title = QLabel()
+            self.color_title.setText("Color")
+            self.color_layout.addWidget(self.color_title)
+            self.color_field = QComboBox()
+            self.color_field.setEditable(False)
+            self.color_field.setMaxVisibleItems(10)
+            self.color_field.setStyleSheet("combobox-popup:0;")
+            for color in TagColor:
+                self.color_field.addItem(color.name, userData=color.value)
+            # self.color_field.setProperty("appearance", "flat")
+            self.color_field.currentIndexChanged.connect(
+                lambda c: (
+                    self.color_field.setStyleSheet(
+                        "combobox-popup:0;"
+                        "font-weight:600;"
+                        f"color:{get_tag_color(ColorType.TEXT, self.color_field.currentData())};"
+                        f"background-color:{get_tag_color(
+                            ColorType.PRIMARY, 
+                            self.color_field.currentData())};"
+                    )
                 )
             )
-        )
-        self.color_layout.addWidget(self.color_field)
+            self.color_layout.addWidget(self.color_field)
 
-        # Add Widgets to Layout ================================================
-        self.root_layout.addWidget(self.name_widget)
-        self.root_layout.addWidget(self.shorthand_widget)
-        self.root_layout.addWidget(self.aliases_widget)
-        self.root_layout.addWidget(self.subtags_widget)
-        self.root_layout.addWidget(self.color_widget)
-        # self.parent().done.connect(self.update_tag)
+            # Add Widgets to Layout ================================================
+            self.root_layout.addWidget(self.name_widget)
+            self.root_layout.addWidget(self.shorthand_widget)
+            self.root_layout.addWidget(self.aliases_widget)
+            self.root_layout.addWidget(self.subtags_widget)
+            self.root_layout.addWidget(self.color_widget)
+            # self.parent().done.connect(self.update_tag)
 
-        # TODO - fill subtags
-        self.subtags: set[int] = set()
-        self.set_tag(tag or Tag(name=""))
+            # TODO - fill subtags
+            self.subtags: set[int] = set()
+            self.set_tag(tag or Tag(name="New Tag"))
+            if tag is None:
+                self.name_field.selectAll()
 
-    def add_subtag_callback(self, tag_id: int):
-        logger.info("add_subtag_callback", tag_id=tag_id)
-        self.subtags.add(tag_id)
-        self.set_subtags()
+        def add_subtag_callback(self, tag_id: int):
+            logger.info("add_subtag_callback", tag_id=tag_id)
+            self.subtags.add(tag_id)
+            self.set_subtags()
 
-    def remove_subtag_callback(self, tag_id: int):
-        logger.info("removing subtag", tag_id=tag_id)
-        self.subtags.remove(tag_id)
-        self.set_subtags()
+        def remove_subtag_callback(self, tag_id: int):
+            logger.info("removing subtag", tag_id=tag_id)
+            self.subtags.remove(tag_id)
+            self.set_subtags()
 
-    def set_subtags(self):
-        while self.scroll_layout.itemAt(0):
-            self.scroll_layout.takeAt(0).widget().deleteLater()
+        def set_subtags(self):
+            while self.scroll_layout.itemAt(0):
+                self.scroll_layout.takeAt(0).widget().deleteLater()
 
-        c = QWidget()
-        layout = QVBoxLayout(c)
-        layout.setContentsMargins(0, 0, 0, 0)
-        layout.setSpacing(3)
-        for tag_id in self.subtags:
-            tag = self.lib.get_tag(tag_id)
-            tw = TagWidget(tag, has_edit=False, has_remove=True)
-            tw.on_remove.connect(lambda t=tag_id: self.remove_subtag_callback(t))
-            layout.addWidget(tw)
-        self.scroll_layout.addWidget(c)
+            c = QWidget()
+            layout = QVBoxLayout(c)
+            layout.setContentsMargins(0, 0, 0, 0)
+            layout.setSpacing(3)
+            for tag_id in self.subtags:
+                tag = self.lib.get_tag(tag_id)
+                tw = TagWidget(tag, has_edit=False, has_remove=True)
+                tw.on_remove.connect(lambda t=tag_id: self.remove_subtag_callback(t))
+                layout.addWidget(tw)
+            self.scroll_layout.addWidget(c)
 
-    def set_tag(self, tag: Tag):
-        logger.info("setting tag", tag=tag)
+        def set_tag(self, tag: Tag):
+            logger.info("setting tag", tag=tag)
 
-        self.name_field.setText(tag.name)
-        self.shorthand_field.setText(tag.shorthand or "")
-        # TODO: Implement aliases
-        # self.aliases_field.setText("\n".join(tag.aliases))
-        self.set_subtags()
-        # select item in self.color_field where the userData value matched tag.color
-        for i in range(self.color_field.count()):
-            if self.color_field.itemData(i) == tag.color:
-                self.color_field.setCurrentIndex(i)
-                break
+            self.name_field.setText(tag.name)
+            self.shorthand_field.setText(tag.shorthand or "")
+            # TODO: Implement aliases
+            # self.aliases_field.setText("\n".join(tag.aliases))
+            self.set_subtags()
+            # select item in self.color_field where the userData value matched tag.color
+            for i in range(self.color_field.count()):
+                if self.color_field.itemData(i) == tag.color:
+                    self.color_field.setCurrentIndex(i)
+                    break
 
-        self.tag = tag
+            self.tag = tag
+        
+        def on_name_changed(self):
+            if not self.name_field.text().strip():
+                self.name_field.setStyleSheet("border: 1px solid red; border-radius: 2px; height: 23px;")
+                self.save_button.setDisabled(True)
+            else:
+                self.name_field.setStyleSheet("")
+                self.save_button.setDisabled(False)
 
-    def build_tag(self) -> Tag:
-        color = self.color_field.currentData() or TagColor.DEFAULT
+        def build_tag(self) -> Tag:
+            color = self.color_field.currentData() or TagColor.DEFAULT
 
-        tag = self.tag
-        if self.name_field.text() == "":
-            tag.name = "New Tag"
-        else:
-            tag.name = self.name_field.text()
-        tag.shorthand = self.shorthand_field.text()
-        tag.color = color
+            tag = self.tag
+            tag.shorthand = self.shorthand_field.text()
+            tag.color = color
 
-        logger.info("built tag", tag=tag)
-        return tag
+            logger.info("built tag", tag=tag)
+            return tag
