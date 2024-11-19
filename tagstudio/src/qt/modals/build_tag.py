7ae2bc24d6ab82586806f2dde271668723c0fc9d
Coolio <softwaredevelopment.stingray177@passinbox.com>
2024-11-19 16:14:34 -0600

    feat(ui): pre-select default tag name in `BuildTagPanel` (#592)
    This changes the behavior of the tag name inside `BuildTagPanel` for newly created tags:
    * The default "New Tag" name is now automatically highlighted
    * Blank tag names (including spaces) are no longer allowed to be created
    * NOTE: This does not change the tag name column rules in the db, nor does it necessarily need to
    
    ***
    
    * [Feature Request]: Make the create tag panel have empty tag name field
    
    * [Feature Request]: Make the create tag panel have empty tag name field
    
    * Revert "[Feature Request]: Make the create tag panel have empty tag name field"
    
    This reverts commit f9c7f5d8892fb70a1618f05554234851a27c7eaa.
    
    * [Feature Request]: Make the create tag panel have empty tag name field
    
    * Revert "[Feature Request]: Make the create tag panel have empty tag name field"
    
    This reverts commit e5df3e0f15a1b97c49390ee6190b906958eddbb1.
    
    * Update .gitignore
    
    * Updated as per disscussion in issue #591 (DRAFT
    
    * Updated as per disscussion in issue #591 (DRAFT
    
    * Added formatting
    
    * Updated code as per discussion is #592
    
    * Updated code as per discussion is #592 (again)
    
    * Fixed spacing
    
    * Add placeholder text to name field.
    
    Co-authored-by: Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
    
    * Use universal red color for red border.
    
    Co-authored-by: Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
    
    * fix: add `src.core.palette` imports
    
    ---------
    
    Co-authored-by: Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>

--- tagstudio/src/qt/modals/build_tag.py
+++ tagstudio/src/qt/modals/build_tag.py
@@ -18,7 +18,7 @@ from PySide6.QtWidgets import (
 )
 from src.core.library import Library, Tag
 from src.core.library.alchemy.enums import TagColor
-from src.core.palette import ColorType, get_tag_color
+from src.core.palette import ColorType, UiColor, get_tag_color, get_ui_color
 from src.qt.modals.tag_search import TagSearchPanel
 from src.qt.widgets.panel import PanelModal, PanelWidget
 from src.qt.widgets.tag import TagWidget
@@ -51,6 +51,9 @@ class BuildTagPanel(PanelWidget):
         self.name_title.setText("Name")
         self.name_layout.addWidget(self.name_title)
         self.name_field = QLineEdit()
+        self.name_field.setFixedHeight(24)
+        self.name_field.textChanged.connect(self.on_name_changed)
+        self.name_field.setPlaceholderText("Tag Name (Required)")
         self.name_layout.addWidget(self.name_field)
 
         # Shorthand ------------------------------------------------------------
@@ -162,6 +165,8 @@ class BuildTagPanel(PanelWidget):
         # TODO - fill subtags
         self.subtags: set[int] = set()
         self.set_tag(tag or Tag(name="New Tag"))
+        if tag is None:
+            self.name_field.selectAll()
 
     def add_subtag_callback(self, tag_id: int):
         logger.info("add_subtag_callback", tag_id=tag_id)
@@ -203,6 +208,18 @@ class BuildTagPanel(PanelWidget):
                 break
 
         self.tag = tag
+
+    def on_name_changed(self):
+        is_empty = not self.name_field.text().strip()
+
+        self.name_field.setStyleSheet(
+            f"border: 1px solid {get_ui_color(ColorType.PRIMARY, UiColor.RED)}; border-radius: 2px"
+            if is_empty
+            else ""
+        )
+
+        if self.panel_save_button is not None:
+            self.panel_save_button.setDisabled(is_empty)
 
     def build_tag(self) -> Tag:
         color = self.color_field.currentData() or TagColor.DEFAULT
