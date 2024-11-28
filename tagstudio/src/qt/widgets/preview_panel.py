2815bf08e407ceb3606e2c28ec4e494f3109792a
Jann Stute <jann.stute@protonmail.com>
2024-11-28 21:35:36 +0100

    partially revert "refix type ignore comments" due to being implemented in #608

--- tagstudio/src/qt/widgets/preview_panel.py
+++ tagstudio/src/qt/widgets/preview_panel.py
@@ -480,7 +480,7 @@ class PreviewPanel(QWidget):
         if filepath and filepath.is_file():
             created: dt = None
             if platform.system() == "Windows" or platform.system() == "Darwin":
-                created = dt.fromtimestamp(filepath.stat().st_birthtime)  # type: ignore[attr-defined,unused-ignore]
+                created = dt.fromtimestamp(filepath.stat().st_birthtime)  # type: ignore[attr-defined]
             else:
                 created = dt.fromtimestamp(filepath.stat().st_ctime)
             modified: dt = dt.fromtimestamp(filepath.stat().st_mtime)
