de7c3f4dfdf237e744ffae59b7eb98bb4734f596
Nginearing <142851004+Nginearing@users.noreply.github.com>
2024-11-30 00:14:38 +0000

    Actually fixing the typo

--- tagstudio/src/qt/widgets/rating.py
+++ tagstudio/src/qt/widgets/rating.py
@@ -24,7 +24,7 @@ class RatingWidget(FieldWidget):
             self.stars[star].setStyleSheet("font-weight: bold; font-size: 20px;")
         for star in range(rating_index + 1, len(self.stars)):
             self.stars[star].setStyleSheet("font-size: 30px;") 
-        rating_indexs += 1
+        rating_index += 1
     # def modify_stars(self, totalstars):
         # for star in self.stars:
             # star.deleteLater()
