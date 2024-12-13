8209e33bc4c6e682ef20b555d348d8caf8f1a6b3
dandydev01 <sandermankelsey@gmail.com>
2024-12-13 13:47:46 -0700

    fix: bug where preview_panel tag was out of date compared to tag in database after edits where made using the tagDatabasePanel

--- tagstudio/src/qt/ts_qt.py
+++ tagstudio/src/qt/ts_qt.py
@@ -656,7 +656,11 @@ class QtDriver(DriverMixin, QObject):
 
     def show_tag_database(self):
         self.modal = PanelModal(
-            TagDatabasePanel(self.lib), "Library Tags", "Library Tags", has_save=False
+            widget=TagDatabasePanel(self.lib), 
+            title="Library Tags", 
+            window_title="Library Tags", 
+            done_callback=self.preview_panel.update_widgets,
+            has_save=False
         )
         self.modal.show()
 
