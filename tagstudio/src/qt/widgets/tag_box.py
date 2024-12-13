02bc6a26badca9537f43a272a3a6a4036a82bf3a
dandydev01 <sandermankelsey@gmail.com>
2024-12-13 13:36:17 -0700

    fix: format

--- tagstudio/src/qt/widgets/tag_box.py
+++ tagstudio/src/qt/widgets/tag_box.py
@@ -139,7 +139,7 @@ class TagBoxWidget(FieldWidget):
         self.edit_modal.saved.connect(
             lambda: self.driver.lib.update_tag(
                 build_tag_panel.build_tag(),
-                subtag_ids=set(build_tag_panel.subtag_ids),               
+                subtag_ids=set(build_tag_panel.subtag_ids),
                 alias_names=set(build_tag_panel.alias_names),
                 alias_ids=set(build_tag_panel.alias_ids),
             )
