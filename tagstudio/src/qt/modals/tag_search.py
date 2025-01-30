2f41e5ac6d5331b0c1ba93f08607f5fd2fac91b3
Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
2025-01-29 16:46:58 -0800

    fix: return consistent search results

--- tagstudio/src/qt/modals/tag_search.py
+++ tagstudio/src/qt/modals/tag_search.py
@@ -222,8 +222,9 @@ class TagSearchPanel(PanelWidget):
                     results_1.append(tag)
                 else:
                     results_2.append(tag)
-            results_1.sort(key=lambda tag: len(tag.name))
-            results_2.sort()
+            results_1.sort(key=lambda tag: self.lib.tag_display_name(tag.id))
+            results_1.sort(key=lambda tag: len(self.lib.tag_display_name(tag.id)))
+            results_2.sort(key=lambda tag: self.lib.tag_display_name(tag.id))
             self.first_tag_id = results_1[0].id if len(results_1) > 0 else tag_results[0].id
             for tag in results_1 + results_2:
                 self.scroll_layout.addWidget(self.__build_row_item_widget(tag))
