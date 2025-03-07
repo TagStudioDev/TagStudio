1b8289889e620a9aaabf024428fdfba6a2a64b91
Jann Stute <jann.stute@protonmail.com>
2025-03-07 22:45:38 +0100

    fix: use existing filename translation instead

--- src/tagstudio/core/library/alchemy/enums.py
+++ src/tagstudio/core/library/alchemy/enums.py
@@ -67,7 +67,7 @@ class ItemType(enum.Enum):
 
 class SortingModeEnum(enum.Enum):
     DATE_ADDED = "file.date_added"
-    FILE_NAME = "file.name"
+    FILE_NAME = "generic.filename"
     PATH = "file.path"
 
 
