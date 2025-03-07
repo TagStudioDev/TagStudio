2073a284fe6b3893940b04f691f3e864a0a52114
Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
2025-03-06 16:22:54 -0800

    fix: separate about screen title from translations (#836)
    * fix: separate about screen title from translations
    
    * chore: format with ruff's evil twin

--- tagstudio/src/qt/modals/about.py
+++ tagstudio/src/qt/modals/about.py
@@ -8,6 +8,7 @@ from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QWidget
 from src.core.constants import VERSION, VERSION_BRANCH
+from src.core.palette import ColorType, UiColor, get_ui_color
 from src.qt.modals.ffmpeg_checker import FfmpegChecker
 from src.qt.resource_manager import ResourceManager
 from src.qt.translations import Translations
@@ -22,9 +23,11 @@ class AboutModal(QWidget):
         self.rm: ResourceManager = ResourceManager()
 
         self.setWindowModality(Qt.WindowModality.ApplicationModal)
-        self.setMinimumSize(400, 500)
+        self.setMinimumSize(360, 480)
         self.root_layout = QVBoxLayout(self)
-        self.root_layout.setContentsMargins(20, 20, 20, 6)
+        self.root_layout.setContentsMargins(24, 24, 24, 6)
+        self.root_layout.setSpacing(12)
+        self.root_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
         self.logo_widget = QLabel()
         self.logo_widget.setObjectName("logo")
@@ -34,29 +37,33 @@ class AboutModal(QWidget):
         )
         self.logo_widget.setPixmap(self.logo_pixmap)
         self.logo_widget.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        self.logo_widget.setContentsMargins(0, 0, 0, 20)
+        self.logo_widget.setContentsMargins(0, 0, 0, 24)
 
         ff_version = self.fc.version()
-        ffmpeg = '<span style="color:red">Missing</span>'
+        red = get_ui_color(ColorType.PRIMARY, UiColor.RED)
+        green = get_ui_color(ColorType.PRIMARY, UiColor.GREEN)
+
+        ffmpeg = f'<span style="color:{red}">Missing</span>'
         if ff_version["ffmpeg"] is not None:
-            ffmpeg = '<span style="color:green">Found</span> (' + ff_version["ffmpeg"] + ")"
-        ffprobe = '<span style="color:red">Missing</span>'
+            ffmpeg = f'<span style="color:{green}">Found</span> (' + ff_version["ffmpeg"] + ")"
+
+        ffprobe = f'<span style="color:{red}">Missing</span>'
         if ff_version["ffprobe"] is not None:
-            ffprobe = '<span style="color:green">Found</span> (' + ff_version["ffprobe"] + ")"
-        self.content_widget = QLabel(
+            ffprobe = f'<span style="color:{green}">Found</span> (' + ff_version["ffprobe"] + ")"
+
+        branch: str = (" (" + VERSION_BRANCH + ")") if VERSION_BRANCH else ""
+        self.title_label = QLabel(f"<h2>TagStudio Alpha {VERSION}{branch}</h2>")
+        self.title_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.content_label = QLabel(
             Translations.format(
-                "about.content",
-                version=VERSION,
-                branch=VERSION_BRANCH,
-                config_path=config_path,
-                ffmpeg=ffmpeg,
-                ffprobe=ffprobe,
+                "about.content", config_path=config_path, ffmpeg=ffmpeg, ffprobe=ffprobe
             )
         )
-        self.content_widget.setObjectName("contentLabel")
-        self.content_widget.setWordWrap(True)
-        self.content_widget.setOpenExternalLinks(True)
-        self.content_widget.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.content_label.setObjectName("contentLabel")
+        self.content_label.setWordWrap(True)
+        self.content_label.setOpenExternalLinks(True)
+        self.content_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
 
         self.button_widget = QWidget()
         self.button_layout = QHBoxLayout(self.button_widget)
@@ -64,10 +71,11 @@ class AboutModal(QWidget):
 
         self.close_button = QPushButton(Translations["generic.close"])
         self.close_button.clicked.connect(lambda: self.close())
-        self.close_button.setMaximumWidth(80)
 
         self.button_layout.addWidget(self.close_button)
 
         self.root_layout.addWidget(self.logo_widget)
-        self.root_layout.addWidget(self.content_widget, Qt.AlignmentFlag.AlignTop)
+        self.root_layout.addWidget(self.title_label)
+        self.root_layout.addWidget(self.content_label)
+        self.root_layout.addStretch(1)
         self.root_layout.addWidget(self.button_widget)
