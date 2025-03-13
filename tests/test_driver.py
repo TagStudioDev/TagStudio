e44316c3e7aa1b6da4190f1c2943b5e84c2f4df0
Jann Stute <jann.stute@protonmail.com>
2025-03-13 15:40:19 +0100

    fix: update test_driver.py

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -7,18 +7,19 @@ from PySide6.QtCore import QSettings
 from tagstudio.core.constants import TS_FOLDER_NAME
 from tagstudio.core.driver import DriverMixin
 from tagstudio.core.enums import SettingItems
+from tagstudio.core.global_settings import GlobalSettings
 from tagstudio.core.library.alchemy.library import LibraryStatus
 
 
 class TestDriver(DriverMixin):
-    def __init__(self, settings):
+    def __init__(self, settings: GlobalSettings, cache: QSettings):
         self.settings = settings
+        self.cached_values = cache
 
 
 def test_evaluate_path_empty():
     # Given
-    settings = QSettings()
-    driver = TestDriver(settings)
+    driver = TestDriver(GlobalSettings(), QSettings())
 
     # When
     result = driver.evaluate_path(None)
@@ -29,8 +30,7 @@ def test_evaluate_path_empty():
 
 def test_evaluate_path_missing():
     # Given
-    settings = QSettings()
-    driver = TestDriver(settings)
+    driver = TestDriver(GlobalSettings(), QSettings())
 
     # When
     result = driver.evaluate_path("/0/4/5/1/")
@@ -41,9 +41,9 @@ def test_evaluate_path_missing():
 
 def test_evaluate_path_last_lib_not_exists():
     # Given
-    settings = QSettings()
-    settings.setValue(SettingItems.LAST_LIBRARY, "/0/4/5/1/")
-    driver = TestDriver(settings)
+    cache = QSettings()
+    cache.setValue(SettingItems.LAST_LIBRARY, "/0/4/5/1/")
+    driver = TestDriver(GlobalSettings(), cache)
 
     # When
     result = driver.evaluate_path(None)
@@ -55,13 +55,16 @@ def test_evaluate_path_last_lib_not_exists():
 def test_evaluate_path_last_lib_present():
     # Given
     with TemporaryDirectory() as tmpdir:
-        settings_file = tmpdir + "/test_settings.ini"
-        settings = QSettings(settings_file, QSettings.Format.IniFormat)
-        settings.setValue(SettingItems.LAST_LIBRARY, tmpdir)
-        settings.sync()
+        cache_file = tmpdir + "/test_settings.ini"
+        cache = QSettings(cache_file, QSettings.Format.IniFormat)
+        cache.setValue(SettingItems.LAST_LIBRARY, tmpdir)
+        cache.sync()
+
+        settings = GlobalSettings()
+        settings.open_last_loaded_on_startup = True
 
         makedirs(Path(tmpdir) / TS_FOLDER_NAME)
-        driver = TestDriver(settings)
+        driver = TestDriver(settings, cache)
 
         # When
         result = driver.evaluate_path(None)
