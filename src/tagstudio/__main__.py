1658471efbbf0542eea065deb910867a5e1ec255
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2025-04-11 12:23:58 +0200

    feat: add minimalist "__main__.py" to 'tagstudio' module

---
+++ src/tagstudio/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# Copyright (C) 2024 Travis Abendshien (CyanVoxel).
+# Licensed under the GPL-3.0 License.
+# Created for TagStudio: https://github.com/CyanVoxel/TagStudio
+
+from tagstudio.main import main
+
+if __name__ == "__main__":
+    main()
