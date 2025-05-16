276803c3fb3621c4b25de54504d3a59be90909b6
Jann Stute <jann.stute@protonmail.com>
2025-05-16 21:09:57 +0200

    refactor: factor out regions into separate methods

--- src/tagstudio/qt/main_window.py
+++ src/tagstudio/qt/main_window.py
@@ -46,13 +46,40 @@ class MainWindow(QMainWindow):
             self.setObjectName("MainWindow")
         self.resize(1300, 720)
 
-        # region Central Widget
+        self.setup_central_widget(driver)
+
+        self.setup_status_bar()
+
+        QMetaObject.connectSlotsByName(self)
+
+        # NOTE: These are old attempts to allow for a translucent/acrylic
+        # window effect. This may be attempted again in the future.
+        # self.setWindowFlag(Qt.WindowType.NoDropShadowWindowHint, True)
+        # self.setWindowFlag(Qt.WindowType.WindowTransparentForInput, False)
+        # # self.setWindowFlag(Qt.WindowType.FramelessWindowHint, True)
+        # self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
+
+        # self.windowFX = WindowEffect()
+        # self.windowFX.setAcrylicEffect(self.winId(), isEnableShadow=False)
+
+    # region UI Setup Methods
+
+    def setup_central_widget(self, driver: "QtDriver"):
         self.central_widget = QWidget(self)
         self.central_widget.setObjectName("centralwidget")
         self.central_layout = QGridLayout(self.central_widget)
         self.central_layout.setObjectName("centralLayout")
 
-        # region Search Bar:        Nav Buttons, Search Field, Search Button
+        self.setup_search_bar()
+
+        self.setup_extra_input_bar()
+
+        self.setup_content(driver)
+
+        self.setCentralWidget(self.central_widget)
+
+    def setup_search_bar(self):
+        """Sets up Nav Buttons, Search Field, Search Button."""
         nav_button_style = "font-size:14;font-weight:bold;"
         self.search_bar_layout = QHBoxLayout()
         self.search_bar_layout.setObjectName("search_bar_layout")
@@ -90,9 +117,9 @@ class MainWindow(QMainWindow):
         self.search_bar_layout.addWidget(self.search_button)
 
         self.central_layout.addLayout(self.search_bar_layout, 3, 0, 1, 1)
-        # endregion
 
-        # region Extra Input Bar:   Inputs for Sorting Settings and Thumbnail Size
+    def setup_extra_input_bar(self):
+        """Sets up inputs for sorting settings and thumbnail size."""
         self.extra_input_layout = QHBoxLayout()
         self.extra_input_layout.setObjectName("extra_input_layout")
 
@@ -125,9 +152,8 @@ class MainWindow(QMainWindow):
         self.extra_input_layout.addWidget(self.thumb_size_combobox)
 
         self.central_layout.addLayout(self.extra_input_layout, 5, 0, 1, 1)
-        # endregion
 
-        # region Content: Entry Scroll List
+    def setup_content(self, driver: "QtDriver"):
         self.content_layout = QHBoxLayout()
         self.content_layout.setObjectName("horizontalLayout")
 
@@ -135,7 +161,14 @@ class MainWindow(QMainWindow):
         self.content_splitter.setObjectName("splitter")
         self.content_splitter.setHandleWidth(12)
 
-        # region Entry List
+        self.setup_entry_list(driver)
+
+        self.content_splitter.setStretchFactor(0, 1)
+        self.content_layout.addWidget(self.content_splitter)
+
+        self.central_layout.addLayout(self.content_layout, 10, 0, 1, 1)
+
+    def setup_entry_list(self, driver: "QtDriver"):
         self.entry_list_container = QWidget()
         self.entry_list_layout = QVBoxLayout(self.entry_list_container)
         self.entry_list_layout.setSpacing(0)
@@ -153,19 +186,9 @@ class MainWindow(QMainWindow):
 
         self.pagination = Pagination()
         self.entry_list_layout.addWidget(self.pagination)
-        # endregion
-
         self.content_splitter.addWidget(self.entry_list_container)
-        self.content_splitter.setStretchFactor(0, 1)
-        self.content_layout.addWidget(self.content_splitter)
 
-        self.central_layout.addLayout(self.content_layout, 10, 0, 1, 1)
-        # endregion
-
-        self.setCentralWidget(self.central_widget)
-        # endregion
-
-        # region Status Bar
+    def setup_status_bar(self):
         self.status_bar = QStatusBar(self)
         self.status_bar.setObjectName("statusbar")
         status_bar_size_policy = QSizePolicy(
@@ -177,19 +200,8 @@ class MainWindow(QMainWindow):
         self.status_bar.setSizePolicy(status_bar_size_policy)
         self.status_bar.setSizeGripEnabled(False)
         self.setStatusBar(self.status_bar)
-        # endregion
 
-        QMetaObject.connectSlotsByName(self)
-
-        # NOTE: These are old attempts to allow for a translucent/acrylic
-        # window effect. This may be attempted again in the future.
-        # self.setWindowFlag(Qt.WindowType.NoDropShadowWindowHint, True)
-        # self.setWindowFlag(Qt.WindowType.WindowTransparentForInput, False)
-        # # self.setWindowFlag(Qt.WindowType.FramelessWindowHint, True)
-        # self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
-
-        # self.windowFX = WindowEffect()
-        # self.windowFX.setAcrylicEffect(self.winId(), isEnableShadow=False)
+    # endregion
 
     def moveEvent(self, event) -> None:  # noqa: N802
         # time.sleep(0.02)  # sleep for 20ms
