cf6c56c9d25bc0b47db43e5a76f372d975f576d6
Jann Stute <46534683+Computerdores@users.noreply.github.com>
2025-06-04 09:29:07 +0200

    fix: restore page navigation state (#933)
    * refactor: store browsing history for navigation purposes
    
    * refactor: remove page_size from FilterState
    
    * refactor: move on from the term "filter" in favor of "BrowsingState"
    
    * fix: refactors didn't propagate to the tests
    
    * fix: ruff complaints
    
    * fix: remaing refactoring errors
    
    * fix: navigation works again
    
    * fix: also store and restore query

--- src/tagstudio/core/utils/dupe_files.py
+++ src/tagstudio/core/utils/dupe_files.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import structlog
 
-from tagstudio.core.library.alchemy.enums import FilterState
+from tagstudio.core.library.alchemy.enums import BrowsingState
 from tagstudio.core.library.alchemy.library import Library
 from tagstudio.core.library.alchemy.models import Entry
 
@@ -52,7 +52,7 @@ class DupeRegistry:
                         continue
 
                     results = self.library.search_library(
-                        FilterState.from_path(path_relative, page_size=500),
+                        BrowsingState.from_path(path_relative), 500
                     )
 
                     if not results:
