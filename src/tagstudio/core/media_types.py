aab5c5e39b420474fd5a8c19abcc1f71ab3e6101
Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
2025-06-04 13:34:56 -0700

    add ".fhdx" and ".ts" filetypes

--- src/tagstudio/core/media_types.py
+++ src/tagstudio/core/media_types.py
@@ -242,7 +242,7 @@ class MediaCategories:
         ".sqlite",
         ".sqlite3",
     }
-    _DISK_IMAGE_SET: set[str] = {".bios", ".dmg", ".iso"}
+    _DISK_IMAGE_SET: set[str] = {".bios", ".dmg", ".fhdx" ".iso"}
     _DOCUMENT_SET: set[str] = {
         ".doc",
         ".docm",
@@ -408,6 +408,7 @@ class MediaCategories:
         ".mp4",
         ".webm",
         ".wmv",
+        ".ts",
     }
 
     ADOBE_PHOTOSHOP_TYPES = MediaCategory(
