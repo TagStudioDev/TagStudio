c100babd9fc80829703d4d2da2985e873e136b02
Jann Stute <46534683+Computerdores@users.noreply.github.com>
2025-06-06 21:45:30 +0200

    feat(ui): option to change tag primary click behavior (#945)
    * feat: add settings field
    
    * feat: change click behaviour based on settings value
    
    * fix: make ignore comment pyright specific to shut up mypy
    
    * fix: add german and english translations for new strings
    
    * fix: settings dropdowns were always english not matter the selected language

--- src/tagstudio/qt/widgets/tag_box.py
+++ src/tagstudio/qt/widgets/tag_box.py
@@ -8,6 +8,7 @@ import typing
 import structlog
 from PySide6.QtCore import Signal
 
+from tagstudio.core.enums import TagClickActionOption
 from tagstudio.core.library.alchemy.enums import BrowsingState
 from tagstudio.core.library.alchemy.models import Tag
 from tagstudio.qt.flowlayout import FlowLayout
@@ -25,6 +26,8 @@ logger = structlog.get_logger(__name__)
 class TagBoxWidget(FieldWidget):
     updated = Signal()
     error_occurred = Signal(Exception)
+
+    driver: "QtDriver"
 
     def __init__(
         self,
@@ -50,11 +53,11 @@ class TagBoxWidget(FieldWidget):
         tags_ = sorted(list(tags), key=lambda tag: self.driver.lib.tag_display_name(tag.id))
         logger.info("[TagBoxWidget] Tags:", tags=tags)
         while self.base_layout.itemAt(0):
-            self.base_layout.takeAt(0).widget().deleteLater()
+            self.base_layout.takeAt(0).widget().deleteLater()  # pyright: ignore[reportOptionalMemberAccess]
 
         for tag in tags_:
             tag_widget = TagWidget(tag, library=self.driver.lib, has_edit=True, has_remove=True)
-            tag_widget.on_click.connect(lambda t=tag: self.edit_tag(t))
+            tag_widget.on_click.connect(lambda t=tag: self.__on_tag_clicked(t))
 
             tag_widget.on_remove.connect(
                 lambda tag_id=tag.id: (
@@ -72,6 +75,26 @@ class TagBoxWidget(FieldWidget):
             )
 
             self.base_layout.addWidget(tag_widget)
+
+    def __on_tag_clicked(self, tag: Tag):
+        match self.driver.settings.tag_click_action:
+            case TagClickActionOption.OPEN_EDIT:
+                self.edit_tag(tag)
+            case TagClickActionOption.SET_SEARCH:
+                self.driver.update_browsing_state(BrowsingState.from_tag_id(tag.id))
+            case TagClickActionOption.ADD_TO_SEARCH:
+                # NOTE: modifying the ast and then setting that would be nicer
+                #       than this string manipulation, but also much more complex,
+                #       due to needing to implement a visitor that turns an AST to a string
+                #       So if that exists when you read this, change the following accordingly.
+                current = self.driver.browsing_history.current
+                suffix = BrowsingState.from_tag_id(tag.id).query
+                assert suffix is not None
+                self.driver.update_browsing_state(
+                    current.with_search_query(
+                        f"{current.query} {suffix}" if current.query else suffix
+                    )
+                )
 
     def edit_tag(self, tag: Tag):
         assert isinstance(tag, Tag), f"tag is {type(tag)}"
