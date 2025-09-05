165a9162259b949ff0a92a241ed546d9107fb4e4
Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
2025-09-05 14:47:51 -0700

    refactor: change names in cache_manager.py to be less ambiguous, more descriptive

--- src/tagstudio/qt/cache_manager.py
+++ src/tagstudio/qt/cache_manager.py
@@ -17,7 +17,7 @@ from tagstudio.core.global_settings import DEFAULT_THUMB_CACHE_SIZE
 logger = structlog.get_logger(__name__)
 
 
-class CacheEntry:
+class CacheFolder:
     def __init__(self, path: Path, size: int):
         self.path: Path = path
         self.size: int = size
@@ -30,40 +30,38 @@ class CacheManager:
         self,
         library_dir: Path,
         max_size: int = DEFAULT_THUMB_CACHE_SIZE,
-        max_folder_size: int = MAX_FOLDER_SIZE,
     ):
         self._lock = RLock()
-        self.cache_folder = library_dir / TS_FOLDER_NAME / THUMB_CACHE_NAME
-        self.max_folder_size = max_folder_size
-        self.max_size = max(max_size, max_folder_size)
+        self.cache_path = library_dir / TS_FOLDER_NAME / THUMB_CACHE_NAME
+        self.max_size = max(max_size, CacheManager.MAX_FOLDER_SIZE)
 
-        self.folders: list[CacheEntry] = []
+        self.folders: list[CacheFolder] = []
         self.current_size = 0
-        if self.cache_folder.exists():
-            for folder in self.cache_folder.iterdir():
+        if self.cache_path.exists():
+            for folder in self.cache_path.iterdir():
                 if not folder.is_dir():
                     continue
                 folder_size = 0
                 for file in folder.iterdir():
                     folder_size += file.stat().st_size
-                self.folders.append(CacheEntry(folder, folder_size))
+                self.folders.append(CacheFolder(folder, folder_size))
                 self.current_size += folder_size
 
-    def _set_mru(self, index: int):
-        """Move entry at index so it's considered the most recently used."""
+    def _set_most_recent_folder(self, index: int):
+        """Move CacheFolder at index so it's considered the most recently used folder."""
         with self._lock as _lock:
             if index == (len(self.folders) - 1):
                 return
-            entry = self.folders.pop(index)
-            self.folders.append(entry)
+            cache_folder = self.folders.pop(index)
+            self.folders.append(cache_folder)
 
-    def _mru(self) -> Iterable[int]:
+    def _get_most_recent_folder(self) -> Iterable[int]:
         """Get each folders index sorted most recently used first."""
         with self._lock as _lock:
             return reversed(range(len(self.folders)))
 
-    def _lru(self) -> Iterable[int]:
-        """Get each folders index sorted least recently used first."""
+    def _least_recent_folder(self) -> Iterable[int]:
+        """Get each folder's index sorted least recently used first."""
         with self._lock as _lock:
             return range(len(self.folders))
 
@@ -78,14 +76,14 @@ class CacheManager:
             self.folders = folders
         logger.info("[CacheManager] Cleared cache!")
 
-    def _remove_folder(self, entry: CacheEntry) -> bool:
+    def _remove_folder(self, cache_folder: CacheFolder) -> bool:
         with self._lock as _lock:
-            self.current_size -= entry.size
-            if not entry.path.is_dir():
+            self.current_size -= cache_folder.size
+            if not cache_folder.path.is_dir():
                 return True
 
             is_empty = True
-            for file in entry.path.iterdir():
+            for file in cache_folder.path.iterdir():
                 assert file.is_file() and file.suffix == ".webp"
                 try:
                     file.unlink(missing_ok=True)
@@ -94,61 +92,61 @@ class CacheManager:
                     logger.warn("[CacheManager] Failed to remove file", file=file, error=e)
 
             if is_empty:
-                entry.path.rmdir()
+                cache_folder.path.rmdir()
                 return True
             else:
                 size = 0
-                for file in entry.path.iterdir():
+                for file in cache_folder.path.iterdir():
                     size += file.stat().st_size
-                entry.size = size
+                cache_folder.size = size
                 self.current_size += size
             return False
 
     def get_file_path(self, file_name: Path) -> Path | None:
         with self._lock as _lock:
-            for i in self._mru():
-                entry = self.folders[i]
-                file_path = entry.path / file_name
+            for i in self._get_most_recent_folder():
+                cache_folder = self.folders[i]
+                file_path = cache_folder.path / file_name
                 if file_path.exists():
-                    self._set_mru(i)
+                    self._set_most_recent_folder(i)
                     return file_path
         return None
 
     def save_image(self, image: Image.Image, file_name: Path, mode: str = "RGBA"):
         """Save an image to the cache."""
         with self._lock as _lock:
-            entry = self._get_current_folder()
-            file_path = entry.path / file_name
+            cache_folder: CacheFolder = self._get_current_folder()
+            file_path = cache_folder.path / file_name
             image.save(file_path, mode=mode)
 
             size = file_path.stat().st_size
-            entry.size += size
+            cache_folder.size += size
             self.current_size += size
             self._cull_folders()
 
-    def _create_folder(self) -> CacheEntry:
+    def _create_folder(self) -> CacheFolder:
         with self._lock as _lock:
-            folder = self.cache_folder / Path(str(math.floor(dt.timestamp(dt.now()))))
+            folder = self.cache_path / Path(str(math.floor(dt.timestamp(dt.now()))))
             try:
                 folder.mkdir(parents=True)
             except FileExistsError:
-                for entry in self.folders:
-                    if entry.path == folder:
-                        return entry
-            entry = CacheEntry(folder, 0)
-            self.folders.append(entry)
-            return entry
+                for cache_folder in self.folders:
+                    if cache_folder.path == folder:
+                        return cache_folder
+            cache_folder = CacheFolder(folder, 0)
+            self.folders.append(cache_folder)
+            return cache_folder
 
-    def _get_current_folder(self) -> CacheEntry:
+    def _get_current_folder(self) -> CacheFolder:
         with self._lock as _lock:
             if len(self.folders) == 0:
                 return self._create_folder()
 
-            for i in self._mru():
-                entry = self.folders[i]
-                if entry.size < self.max_folder_size:
-                    self._set_mru(i)
-                    return entry
+            for i in self._get_most_recent_folder():
+                cache_folder: CacheFolder = self.folders[i]
+                if cache_folder.size < CacheManager.MAX_FOLDER_SIZE:
+                    self._set_most_recent_folder(i)
+                    return cache_folder
 
             return self._create_folder()
 
@@ -159,10 +157,12 @@ class CacheManager:
                 return
 
             removed: list[int] = []
-            for i in self._lru():
-                entry = self.folders[i]
-                logger.info("[CacheManager] Removing folder due to size limit", folder=entry.path)
-                if self._remove_folder(entry):
+            for i in self._least_recent_folder():
+                cache_folder: CacheFolder = self.folders[i]
+                logger.info(
+                    "[CacheManager] Removing folder due to size limit", folder=cache_folder.path
+                )
+                if self._remove_folder(cache_folder):
                     removed.append(i)
                 if self.current_size < self.max_size:
                     break
