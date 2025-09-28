d20be9a3d86ba57b6098225aebc70409c8335d4a
Sola-ris <190788035+Sola-ris@users.noreply.github.com>
2025-09-28 12:09:55 +0200

    feat: render .mdp thumbnails.

--- src/tagstudio/qt/previews/renderer.py
+++ src/tagstudio/qt/previews/renderer.py
@@ -7,9 +7,11 @@ import contextlib
 import hashlib
 import math
 import os
+import struct
 import tarfile
 import xml.etree.ElementTree as ET
 import zipfile
+import zlib
 from copy import deepcopy
 from io import BytesIO
 from pathlib import Path
@@ -1378,6 +1380,48 @@ class ThumbRenderer(QObject):
             logger.error("Couldn't render thumbnail", filepath=filepath, error=type(e).__name__)
         return im
 
+    @staticmethod
+    def _mdp_thumb(filepath: Path) -> Image.Image | None:
+        """Extract the thumbnail from a .mdp file.
+
+        Args:
+            filepath (Path): The path of the .mdp file.
+
+        Returns:
+            Image: The embedded thumbnail.
+        """
+        im: Image.Image | None = None
+        try:
+            with open(filepath, "rb") as f:
+                magic = struct.unpack("<7sx", f.read(8))[0]
+                if magic != b"mdipack":
+                    return im
+
+                bin_header = struct.unpack("<LLL", f.read(12))
+                xml_header = ET.fromstring(f.read(bin_header[1]))
+                mdibin_count = len(xml_header.findall("./*Layer")) + 1
+                for _ in range(mdibin_count):
+                    pac_header = struct.unpack("<3sxLLLL48s64s", f.read(132))
+                    if not pac_header[6].startswith(b"thumb"):
+                        f.seek(pac_header[3], os.SEEK_CUR)
+                        continue
+
+                    thumb_element = unwrap(xml_header.find("Thumb"))
+                    dimensions = (
+                        int(unwrap(thumb_element.get("width"))),
+                        int(unwrap(thumb_element.get("height"))),
+                    )
+                    thumb_blob = f.read(pac_header[3])
+                    if pac_header[2] == 1:
+                        thumb_blob = zlib.decompress(thumb_blob, bufsize=pac_header[4])
+
+                    im = Image.frombytes("RGBA", dimensions, thumb_blob, "raw", "BGRA")
+                    break
+        except Exception as e:
+            logger.error("Couldn't render thumbnail", filepath=filepath, error=type(e).__name__)
+
+        return im
+
     def render(
         self,
         timestamp: float,
@@ -1704,6 +1748,9 @@ class ThumbRenderer(QObject):
                     ext, MediaCategories.PDF_TYPES, mime_fallback=True
                 ):
                     image = self._pdf_thumb(_filepath, adj_size)
+                # MDIPACK ======================================================
+                elif MediaCategories.is_ext_in_category(ext, MediaCategories.MDIPACK_TYPES):
+                    image = self._mdp_thumb(_filepath)
                 # No Rendered Thumbnail ========================================
                 if not image:
                     raise NoRendererError
