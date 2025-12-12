30e0a31f77d3e2e679cdb20b303370cda10d5b23
TrigamDev <trigambusiness@gmail.com>
2025-12-12 14:47:09 -0500

    Add syntax cheatsheet modal and markdown widget

---
+++ src/tagstudio/qt/views/search_syntax_modal.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QFrame, QHBoxLayout, QPushButton, QScrollArea, QVBoxLayout, QWidget
+
+from tagstudio.qt.translations import Translations
+from tagstudio.qt.views.markdown_widget_view import MarkdownWidgetView
+
+if TYPE_CHECKING:
+    from tagstudio.qt.ts_qt import QtDriver
+
+
+class SearchSyntaxModal(QWidget):
+    def __init__(self, driver: "QtDriver"):
+        super().__init__()
+
+        # Modal
+        self.setWindowTitle(Translations["search_syntax.title"])
+
+        self.setWindowModality(Qt.WindowModality.ApplicationModal)
+        self.setMinimumSize(360, 540)
+        self.__root_layout = QVBoxLayout(self)
+        self.__root_layout.setSpacing(0)
+        self.__root_layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignCenter)
+
+        # Scroll
+        self.__scroll_area = QScrollArea()
+        self.__scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.__scroll_area.setWidgetResizable(True)
+        self.__scroll_area.setFrameShadow(QFrame.Shadow.Plain)
+        self.__scroll_area.setFrameShape(QFrame.Shape.NoFrame)
+        self.__root_layout.addWidget(self.__scroll_area)
+
+        # Content
+        self.__content: MarkdownWidgetView = MarkdownWidgetView(driver)
+        self.__content.set_file(Path("search_syntax_cheatsheet.md"))
+        self.__content.setWordWrap(True)
+        self.__scroll_area.setWidget(self.__content)
+
+        # Close button
+        self.__buttons_row = QWidget()
+        self.__buttons_row_layout = QHBoxLayout(self.__buttons_row)
+        self.__buttons_row_layout.setContentsMargins(12, 12, 12, 12)
+        self.__buttons_row_layout.addStretch(1)
+        self.__root_layout.addWidget(self.__buttons_row)
+
+        self.__close_button = QPushButton(Translations["generic.close"])
+        self.__close_button.clicked.connect(lambda: self.close())
+        self.__buttons_row_layout.addWidget(self.__close_button)
---
+++ src/tagstudio/resources/markdown/en/search_syntax_cheatsheet.md
@@ -0,0 +1,62 @@
+# Search Syntax
+For a more detailed explanation of search syntax, check [the documentation](https://docs.tagstud.io/search/).
+
+## Boolean Operators
+
+Search for entries that have Tag1 **AND** Tag2
+- Tag1 Tag2
+- Tag1 `AND` Tag2
+
+Search for entries that have Tag1 **OR** Tag2
+- Tag1 `OR` Tag2
+
+Search for entries that **don't** have Tag1
+- `NOT` Tag1
+
+Searches can be grouped and nested by using parentheses to surround parts of your search query.
+- (Tag1 `OR` Tag2) `AND` Tag3
+
+## Escaping Characters
+
+To escape most search terms, surround the section of your search in plain quotes or replace spaces in tag names with underscores.
+- "Tag Name With Spaces"
+- Tag_Name_With_Spaces
+
+## Tags
+
+Search for entries that have the tag Tag1
+- Tag1
+- tag: Tag1
+
+Search for entries that have the tag with ID 1
+- tag_id: 1
+
+## Path
+
+Search for a file named `file.jpg`
+- path: file.jpg
+
+Search for any `.jpg` file
+- path: *.jpg
+
+Search for any file that ends in a number
+- path: *2.*
+
+Search for a file located at `folder/file.jpg`
+- path: folder/file.jpg
+
+Search for any file inside `folder/`
+- path: folder/*
+
+## File/Media Type
+
+Search for videos
+- mediatype: video
+
+Search for `.mp4` files
+- filetype: mp4
+
+## Special Searches
+
+Search for entries that don't contain any tags
+- special: untagged
