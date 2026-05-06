8178193a562142612a21f14f81b9289a97936667
TrigamDev <trigambusiness@gmail.com>
2026-05-06 18:13:17 -0400

    refactor: add type hints

--- src/tagstudio/qt/controllers/field_container_controller.py
+++ src/tagstudio/qt/controllers/field_container_controller.py
@@ -14,37 +14,37 @@ logger = structlog.get_logger(__name__)
 
 
 class FieldContainer(FieldContainerView):
-    copy = Signal()
-    edit = Signal()
-    remove = Signal()
+    copy: Signal = Signal()
+    edit: Signal = Signal()
+    remove: Signal = Signal()
 
     def __init__(self, title: str = "Field", inline: bool = True) -> None:
         super().__init__(title, inline)
 
-    def _copy_callback(self):
+    def _copy_callback(self) -> None:
         self.copy.emit()
 
-    def _edit_callback(self):
+    def _edit_callback(self) -> None:
         self.edit.emit()
 
-    def _remove_callback(self):
+    def _remove_callback(self) -> None:
         self.remove.emit()
 
-    def on_copy(self, callback: Callable[[], None] | None = None):
+    def on_copy(self, callback: Callable[[], None] | None = None) -> None:
         if callback is None:
             return
 
         self.copy.connect(callback)
         self.copy_enabled = True
 
-    def on_edit(self, callback: Callable[[], None] | None = None):
+    def on_edit(self, callback: Callable[[], None] | None = None) -> None:
         if callback is None:
             return
 
         self.edit.connect(callback)
         self.edit_enabled = True
 
-    def on_remove(self, callback: Callable[[], None] | None = None):
+    def on_remove(self, callback: Callable[[], None] | None = None) -> None:
         if callback is None:
             return
 
