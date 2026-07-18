15d21fe6f8787dc49d97ec3e02273c36d951edb1
Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
2026-07-18 02:40:43 -0700

    refactor: code cleanup and fixes

--- src/tagstudio/qt/controllers/suggest_box_controller.py
+++ src/tagstudio/qt/controllers/suggest_box_controller.py
@@ -6,9 +6,10 @@ from typing import TYPE_CHECKING, Any, override
 import structlog
 from PySide6.QtCore import Signal
 from PySide6.QtGui import QShowEvent
-from PySide6.QtWidgets import QGraphicsOpacityEffect, QVBoxLayout
+from PySide6.QtWidgets import QGraphicsOpacityEffect, QWidget
 
 from tagstudio.core.library.alchemy.library import Library
+from tagstudio.qt.controllers.autofill_line_edit import QtCore, QtGui
 from tagstudio.qt.views.panel_modal import PanelWidget
 from tagstudio.qt.views.stylesheets.stylesheets import (
     autofill_line_edit_style,
@@ -41,92 +42,55 @@ def _item_name(item: object) -> str:
         raise AttributeError()
 
 
-class SuggestBox[T](PanelWidget):
+class SuggestBox[T](QWidget):
     item_chosen = Signal(int)
     done = Signal()
     tags_updated = Signal()
 
-    def __init__(
-        self, view: SuggestBoxView, exclude: list[int] | None = None, is_chooser: bool = True
-    ) -> None:
+    def __init__(self, driver: "QtDriver", view: SuggestBoxView) -> None:
         super().__init__()
-        self.view = view
-        self.is_chooser = is_chooser
-        self._layout = QVBoxLayout(self)
-        self._layout.setContentsMargins(0, 0, 0, 0)
-        self._layout.addWidget(self.view)
-        self._driver: QtDriver | None = None
-        self.exclude: list[int] = exclude or []
-        self.added: list[int] = exclude or []
-        self.create_and_add_button_in_layout: bool = False
-        self.limit = 5
-        self.shift_held = False
-
-        # Limits
-        self._unlimited_limit_item_label: str = "All Items"
-        self._limit_items: list[tuple[str, int]] = [
-            ("25", 25),
-            ("50", 50),
-            ("100", 100),
-            ("250", 250),
-            ("500", 500),
-            (self._unlimited_limit_item_label, -1),
-        ]
-        self._default_limit_index: int = 0
-        self._previous_limit_index: int = self._default_limit_index
-
-        # Items
+        self._layout = view
+        self._driver = driver
+        self._limit = 5
+        self._is_shift_held = False
         self._search_results: list[T] = []
+        self.added: list[int] = []
+        self.excluded: list[int] = []
 
-        self._create_and_add_button_label_key: str = ""
-        self.connect_callbacks()
+        self.setLayout(self._layout)
+        self._connect_callbacks()
 
-    def connect_callbacks(self) -> None:
-        self.view.search_field.textChanged.connect(self.on_search_query_changed)
-        self.view.search_field.editingFinished.connect(self.test_editing_finished)
-        self.view.search_field.return_pressed.connect(
-            lambda: self.on_search_query_submitted(self.view.search_field.text())
+    def _connect_callbacks(self) -> None:
+        self._layout.search_field.textChanged.connect(self.on_search_query_changed)
+        self._layout.search_field.editingFinished.connect(self.test_editing_finished)
+        self._layout.search_field.return_pressed.connect(
+            lambda: self.on_search_query_submitted(self._layout.search_field.text())
         )
-        self.view.search_field.shift_return_pressed.connect(
+        self._layout.search_field.shift_return_pressed.connect(
             lambda: self.on_search_query_submitted(
-                self.view.search_field.text(), always_create=True
+                self._layout.search_field.text(), always_create=True
             )
         )
 
-        self.view.search_field.shift_holding.connect(lambda held: self.on_shift_held(held))
+        self._layout.search_field.shift_holding.connect(lambda held: self._on_shift_held(held))
 
-    def on_shift_held(self, held: bool):
+    def _on_shift_held(self, held: bool):
         if held:
-            self.shift_held = True
+            self._is_shift_held = True
             opacity_effect = QGraphicsOpacityEffect(self)
             opacity_effect.setOpacity(0.3)
-            if self.view.content_layout.count() > 0:
-                self.view.content_layout.itemAt(0).widget().setGraphicsEffect(opacity_effect)
+            if self._layout.content_layout.count() > 0:
+                self._layout.content_layout.itemAt(0).widget().setGraphicsEffect(opacity_effect)
         else:
-            self.shift_held = False
-            if self.view.content_layout.count() > 0:
-                self.view.content_layout.itemAt(0).widget().setGraphicsEffect(None)  # pyright: ignore[reportArgumentType]
-
-    def focus_search_box(self, select_all: bool = False) -> None:
-        if not self.isHidden():
-            self.view.search_field.setFocus()
-        if select_all:
-            self.view.search_field.selectAll()
+            self._is_shift_held = False
+            if self._layout.content_layout.count() > 0:
+                self._layout.content_layout.itemAt(0).widget().setGraphicsEffect(None)  # pyright: ignore[reportArgumentType]
 
     def clear_search_query(self) -> None:
-        self.view.search_field.setText("")
+        self._layout.search_field.setText("")
 
     def get_item_widget(self, index: int, library: Library) -> Any:  # pyright: ignore[reportExplicitAny]
         return self.get_item_widget(index, library)
-
-    def set_driver(self, driver: "QtDriver") -> None:
-        self._driver = driver
-
-    def _get_previous_limit(self) -> tuple[str, int]:
-        return self._limit_items[self._previous_limit_index]
-
-    def _get_max_limit(self) -> int:
-        raise NotImplementedError()
 
     def on_search_query_changed(self, query: str) -> None:
         self.update_items(query)
@@ -136,38 +100,35 @@ class SuggestBox[T](PanelWidget):
         logger.info("Query submitted")
         if not query:
             self.done.emit()
-            self.disappear()
+            self.hide_and_reset()
             return
         elif not self.isHidden():
-            self.view.search_field.setFocus()
+            self._layout.search_field.setFocus()
 
         # Create and add item if no search results
         if (len(self._search_results) <= 0) or always_create:
-            self.on_item_create(add_to_entry=True)
-        elif self.is_chooser:
+            self.on_item_create()
+        else:
             self._on_item_chosen(self._search_results[0])
 
         self.clear_search_query()
         self.update_items()
 
-    def on_item_create(self, add_to_entry: bool = False) -> None:  # pyright: ignore[reportUnusedParameter]
+    def on_item_create(self) -> None:
         raise NotImplementedError()
 
     def on_item_edit(self, item: T) -> None:  # pyright: ignore[reportUnusedParameter]
-        raise NotImplementedError()
-
-    def _on_item_remove(self, item: T) -> None:  # pyright: ignore[reportUnusedParameter]
         raise NotImplementedError()
 
     def _on_item_chosen(self, item: T) -> None:  # pyright: ignore[reportUnusedParameter]
         raise NotImplementedError()
 
     def _is_excluded(self, item: T) -> bool:
-        return _item_id(item) in self.exclude
+        return _item_id(item) in self.excluded
 
     def update_items(self, query: str | None = None) -> None:
         """Update the item list given a search query."""
-        logger.info("[SearchPanel] Updating items", limit=self.limit)
+        logger.info("[SearchPanel] Updating items", limit=self._limit)
 
         # Get results for the search query
         query_lower = "" if not query else query.lower()
@@ -199,24 +160,24 @@ class SuggestBox[T](PanelWidget):
                 all_results.remove(item)
         all_results = all_results + already_added
 
-        if self.limit > 0:
-            all_results = all_results[: self.limit]
+        if self._limit > 0:
+            all_results = all_results[: self._limit]
 
         self._search_results = all_results
         logger.info("[SearchPanel] Search results", results=self._search_results)
 
-        for i in range(0, self.limit):
+        for i in range(0, self._limit):
             item: T | None = all_results[i] if i < len(all_results) else None
             self.set_item_widget(item=item, index=i)
 
-        if self.view.content_layout.isEmpty():
-            self.view.scroll_area.setHidden(True)
-            self.view.content_layout.setContentsMargins(0, 0, 0, 0)
-            self.view.search_field.setStyleSheet(autofill_line_edit_style())
+        if self._layout.content_layout.isEmpty():
+            self._layout.scroll_area.setHidden(True)
+            self._layout.content_layout.setContentsMargins(0, 0, 0, 0)
+            self._layout.search_field.setStyleSheet(autofill_line_edit_style())
         else:
-            self.view.scroll_area.setHidden(False)
-            self.view.content_layout.setContentsMargins(6, 6, 6, 6)
-            self.view.search_field.setStyleSheet(autofill_line_edit_top_style())
+            self._layout.scroll_area.setHidden(False)
+            self._layout.content_layout.setContentsMargins(6, 6, 6, 6)
+            self._layout.search_field.setStyleSheet(autofill_line_edit_top_style())
 
     def search_items(self, query: str) -> tuple[list[T], list[T]]:  # pyright: ignore[reportUnusedParameter]
         raise NotImplementedError()
@@ -224,27 +185,41 @@ class SuggestBox[T](PanelWidget):
     def set_item_widget(self, item: T | None, index: int) -> None:  # pyright: ignore[reportUnusedParameter]
         raise NotImplementedError()
 
-    @override
-    def showEvent(self, event: QShowEvent) -> None:
-        self.update_items()
-        self.on_shift_held(held=False)
-        self.clear_search_query()
-        return super().showEvent(event)
-
     def test_editing_finished(self):
         logger.info("Editing finished")
         self.tags_updated.emit()
-        if self.view.search_field.text() == "":
+        if self._layout.search_field.text() == "":
             self.done.emit()
-            self.disappear()
+            self.hide_and_reset()
 
-    def disappear(self):
+    def hide_and_reset(self):
         self.hide()
-        self.view.search_field.setDisabled(True)
-        self.on_shift_held(held=False)
+        self._layout.search_field.setDisabled(True)
+        self._on_shift_held(held=False)
 
-    def create_item(self, edit_item_panel: PanelWidget, choose_item: bool = False) -> None:  # pyright: ignore[reportUnusedParameter]
+    def create_item_from_modal(self, edit_item_panel: PanelWidget) -> None:  # pyright: ignore[reportUnusedParameter]
         raise NotImplementedError()
 
     def edit_item(self, edit_item_panel: PanelWidget) -> None:  # pyright: ignore[reportUnusedParameter]
         raise NotImplementedError()
+
+    @override
+    def showEvent(self, event: QShowEvent) -> None:
+        self.update_items()
+        self._on_shift_held(held=False)
+        self.clear_search_query()
+        return super().showEvent(event)
+
+    @override
+    def layout(self) -> SuggestBoxView:
+        return self._layout
+
+    @override
+    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
+        # When Escape is pressed, focus back on the search box.
+        if event.key() in {
+            QtCore.Qt.Key.Key_Escape,
+            QtCore.Qt.Key.Key_Enter,
+            QtCore.Qt.Key.Key_Return,
+        }:
+            self.hide_and_reset()
