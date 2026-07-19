87f818847e3ddc6f6081e4f64dbd53218f27462b
Travis Abendshien <46939827+CyanVoxel@users.noreply.github.com>
2026-07-18 18:30:26 -0700

    Revert "refactor: remove controller suffix from preview_panel.py"
    This reverts commit c8ba9b15c23b400c29efc175838bd0e5f312dc8b.

--- src/tagstudio/qt/views/main_window.py
+++ src/tagstudio/qt/views/main_window.py
@@ -37,7 +37,7 @@ from PySide6.QtWidgets import (
 
 from tagstudio.core.enums import ShowFilepathOption
 from tagstudio.core.library.alchemy.enums import SortingModeEnum
-from tagstudio.qt.controllers.preview_panel import PreviewPanel
+from tagstudio.qt.controllers.preview_panel_controller import PreviewPanel
 from tagstudio.qt.helpers.color_overlay import auto_theme_overlay
 from tagstudio.qt.mixed.landing import LandingWidget
 from tagstudio.qt.mixed.pagination import Pagination
